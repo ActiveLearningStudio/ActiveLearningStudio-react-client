49ff837a0f53129d2d6a014128c2a63dcd51b0ab
Jawad Raza <razajawad840@gmail.com>
2020-04-26 01:46:19 +0500

    Playlists routes updated

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter, HashRouter as Router, Route, Switch } from "react-router-dom";
 import {createBrowserHistory} from "history";
 
 import PublicRoute from "./PublicRoute";
@@ -8,23 +8,24 @@ import LoginPage from "./../containers/LoginPage";
 import HomePage from "./../containers/HomePage";
 import EditorPage from "./../containers/EditorPage";
 import { TinyEditor } from "../containers/TinyEditor";
+import PrivateRoute from "./PrivateRoute";
 
 const history = createBrowserHistory();
 
 const AppRouter = () => (
   <Router history={history}>
     <Switch>
-      <Route path="/" exact component={HomePage} />
+      <PrivateRoute path="/" exact component={HomePage} />
       <Route path="/playlist/create" render={(props) => <HomePage {...props} openCreatePopup={true} />} />
-      <Route path="/playlist/editor/create/question" render={(props) => {
+      {/* <Route path="/playlist/activity/create/question" render={(props) => {
           //do your console log or temporary testing stuff here
           return <HomePage {...props} />
-      }} />
-      <Route path="/playlist/editor/create" exact component={HomePage} />
-      <Route path="/playlist/editor/create/description" exact component={HomePage} />
-      <Route path="/playlist/editor" exact component={EditorPage} />
+      }} /> */}
+      <Route path="/playlist/activity/create/:id" exact component={HomePage} />
+      {/* <Route path="/playlist/activity/create/description" exact component={HomePage} /> */}
+      {/* <Route path="/playlist/activity" exact component={EditorPage} /> */}
       <Route path="/tiny" exact component={TinyEditor} />
-      <PublicRoute path="/signin" component={LoginPage} />
+      <PublicRoute path="/login" component={LoginPage} />
     </Switch>
   </Router>
 );
