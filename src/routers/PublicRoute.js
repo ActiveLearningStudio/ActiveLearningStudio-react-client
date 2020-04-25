85baf1dbc5cdcdb89767247eec1399d5fd93534a
Jawad Raza <razajawad840@gmail.com>
2020-04-26 01:46:50 +0500

    redux support added

--- src/routers/PublicRoute.js
+++ src/routers/PublicRoute.js
@@ -1,26 +1,32 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Route, Redirect } from "react-router-dom";
+import { Route, Redirect, withRouter } from "react-router-dom";
 
 
 
 const PublicRoute = ({ component: Component, isAuthenticated, ...rest }) => (
   <Route
     {...rest}
-    render={props =>
-      isAuthenticated ? (
-        <Redirect to="/" />
-      ) : (
-        <div>
-          <Component {...props} />
-        </div>
+    render={props => {
+      return (
+        isAuthenticated ? (
+          <Redirect to="/" />
+        ) : (
+          <div>
+            <Component {...props} />
+          </div>
+        )
       )
+    }
+      
     }
   />
 );
 
-const mapStateToProps = state => ({
-  isAuthenticated: !!state.auth.id
-});
+const mapStateToProps = state => {
+  return ({
+    isAuthenticated: !!state.auth.id
+  });
+};
 
-export default connect(mapStateToProps)(PublicRoute);
+export default withRouter(connect(mapStateToProps,null)(PublicRoute));
