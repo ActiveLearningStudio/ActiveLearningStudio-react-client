5354a3b3269a134d27725e3806f7de443d08defc
Jawad Raza <razajawad840@gmail.com>
2020-05-04 20:38:47 +0500

    docker updates

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, HashRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Router, Route, Switch } from "react-router-dom";
 import {createBrowserHistory} from "history";
 
 import PublicRoute from "./PublicRoute";
