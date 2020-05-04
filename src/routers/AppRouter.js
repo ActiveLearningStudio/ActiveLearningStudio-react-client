793e06b37de0a6461f695e013e651eb422a13ed2
Jawad Raza <razajawad840@gmail.com>
2020-05-05 01:04:56 +0500

    Changed names

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -23,17 +23,17 @@ const AppRouter = () => (
     <Switch>
 
       <PrivateRoute path="/" exact component={ProjectsPage} />
-      <PrivateRoute path="/project/preview/:projectid" render={(props) => <ProjectsPage {...props} showPreview={props.match.params.projectid} />} />
-      <PrivateRoute path="/project/preview2/:projectid" render={(props) => <PreviewPage {...props} />} />
+      <Route path="/project/preview/:projectid" render={(props) => <ProjectsPage {...props} showPreview={props.match.params.projectid} />} />
+      <Route path="/project/preview2/:projectid" render={(props) => <PreviewPage {...props} />} />
 
-      <PrivateRoute path="/resource/preview/:resourceid" render={(props) => <PreviewPage {...props} key={props.match.params.resourceid} previewType="resource" />} />
-      <PrivateRoute path="/playlist/preview/:playlistid" render={(props) => <PreviewPage {...props} previewType="playlist" />} />
+      <Route path="/resource/preview/:resourceid" render={(props) => <PreviewPage {...props} key={props.match.params.resourceid} previewType="resource" />} />
+      <Route path="/playlist/preview/:playlistid" render={(props) => <PreviewPage {...props} previewType="playlist" />} />
 
-      <PrivateRoute path="/project/create" render={(props) => <ProjectsPage {...props} showCreateProjectPopup={true} />} />
-      <PrivateRoute path="/project/:projectid" exact component={PlaylistsPage} />
-      <PrivateRoute path="/project/:projectid/playlist/create" render={(props) => <PlaylistsPage {...props} openCreatePopup={true} />} />
-      <PrivateRoute path="/project/:projectid/playlist/:playlistid/activity/create" exact render={(props) => <PlaylistsPage {...props} openCreateResourcePopup={true} />} />
-      <PrivateRoute path="/activities/:activityid" render={(props) => <PlaylistsPage {...props} />} />
+      <Route path="/project/create" render={(props) => <ProjectsPage {...props} showCreateProjectPopup={true} />} />
+      <Route path="/project/:projectid" exact component={PlaylistsPage} />
+      <Route path="/project/:projectid/playlist/create" render={(props) => <PlaylistsPage {...props} openCreatePopup={true} />} />
+      <Route path="/project/:projectid/playlist/:playlistid/activity/create" exact render={(props) => <PlaylistsPage {...props} openCreateResourcePopup={true} />} />
+      <Route path="/activities/:activityid" render={(props) => <PlaylistsPage {...props} />} />
       
       
       
