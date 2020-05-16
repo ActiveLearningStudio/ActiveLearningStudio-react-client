c7e3585eadd29c7e05eac7164786a4556e0c2a35
Jawad Raza <razajawad840@gmail.com>
2020-05-16 16:57:56 +0500

    server cluster added

--- server.js
+++ server.js
@@ -1,15 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser')
-const path = require('path');
-const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
+const compression = require('compression')
+const cluster = require('cluster')
+const path = require('path')
+var logger = require('morgan');
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
-app.listen(process.env.PORT || 8080);
+
+var port    = 8080;
+// var root    = path.dirname( __dirname );
+var cCPUs   = require('os').cpus().length;
+
+if( cluster.isMaster ) {
+  // Create a worker for each CPU
+  for( var i = 0; i < cCPUs; i++ ) {
+    cluster.fork();
+  }
+
+  cluster.on( 'online', function( worker ) {
+    console.log( 'Worker ' + worker.process.pid + ' is online.' );
+  });
+  cluster.on( 'exit', function( worker, code, signal ) {
+    console.log( 'worker ' + worker.process.pid + ' died.' );
+  });
+}
+else {
+  var app    = express();
+  app.use(logger('combined'));
+  app.use(compression())
+  app.use(express.static(path.join(__dirname, 'build')));
+  app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+
+  app
+    .use( bodyParser )
+    .listen( port );
+}
