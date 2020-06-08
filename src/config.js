c8889770779436a68be085493e179fb6bc98f563
Muhammad Qamar Aftab <muhammad_qamar_aftab@snl.int>
2020-06-08 20:00:42 +0500

    css styling and title dynamic issue resolved

--- src/config.js
+++ src/config.js
@@ -1,9 +1,8 @@
 export default global.config = {
-    // h5pAjaxUrl: 'http://localhost:8082',
-    // h5pBaseUrl: 'http://localhost' ,
-    h5pAjaxUrl: 'http://localhost:8082/h5papi',
-    h5pBaseUrl: 'http://localhost' ,
-    laravelAPIUrl: 'http://localhost:8082/api'
-    // h5pExternalAjaxUrl: 'http://localhost:8082/h5papi'
-
+  // h5pAjaxUrl: 'http://localhost:8082',
+  // h5pBaseUrl: 'http://localhost' ,
+  h5pAjaxUrl: "http://192.168.99.100:8082/h5papi",
+  h5pBaseUrl: "http://192.168.99.100",
+  laravelAPIUrl: "http://192.168.99.100:8082/api",
+  // h5pExternalAjaxUrl: 'http://localhost:8082/h5papi'
 };
