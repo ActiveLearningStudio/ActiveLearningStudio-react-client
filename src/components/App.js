43d58859481265af716465b2d58e077cc574fd8f
muhammadqamar <muhammadqamar111@gmail.com>
2020-06-30 21:24:19 +0500

    GA tag added

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
-import './../styles/App.css';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from '@apollo/react-hooks';
-import Home from './Home'
-import './../App.scss'
-import { Helmet } from 'react-helmet';
-
-
-import AppRouter from './../routers/AppRouter'
-
-
-
+import "./../styles/App.css";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+import Home from "./Home";
+import "./../App.scss";
+import { Helmet } from "react-helmet";
+import ReactGA from "react-ga";
+import AppRouter from "./../routers/AppRouter";
+const trackingId = "UA-1841781-13";
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize(trackingId, {
+      debug: true,
+    });
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  });
   return (
     <div>
       <Helmet>
-          <meta name="description" content="CurrikiStudio" />
-          <meta name="theme-color" content="#008f68" />
+        <meta name="description" content="CurrikiStudio" />
+        <meta name="theme-color" content="#008f68" />
       </Helmet>
       <AppRouter />
     </div>
--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware, compose  } from 'redux'
-import { Provider } from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-import './config';
+import "./config";
 
+import "./index.css";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
 
-import './index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
-
-import rootReducer from './reducers'
+import rootReducer from "./reducers";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>, document.getElementById('root'));
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
