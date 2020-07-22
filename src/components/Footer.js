5f719594626d92c954ff51631444c26110f1aa50
muhammadqamar <muhammadqamar111@gmail.com>
2020-07-22 20:32:45 +0500

    fiiter added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,11 @@ import React from "react";
 function Footer() {
   return (
     <footer className="footer-all">
-      <a target="_blank" href="https://www.curriki.org/privacy-policy/">
-        Privacy Policy
-      </a>
       <a target="_blank" href="https://www.curriki.org/terms-of-service/">
         Terms of Service
+      </a>
+      <a target="_blank" href="https://www.curriki.org/privacy-policy/">
+        Privacy Policy
       </a>
     </footer>
   );
