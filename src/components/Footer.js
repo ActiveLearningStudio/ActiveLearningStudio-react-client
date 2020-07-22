5d6f694309c3d795dbf6783e05400aca4fe73971
muhammadqamar <muhammadqamar111@gmail.com>
2020-07-22 20:30:19 +0500

    footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="footer-all">
       <a target="_blank" href="https://www.curriki.org/privacy-policy/">
-        Privacy policy
+        Privacy Policy
       </a>
       <a target="_blank" href="https://www.curriki.org/terms-of-service/">
         Terms of Service
