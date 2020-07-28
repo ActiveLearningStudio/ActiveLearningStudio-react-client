623e3d2bf529c5585b8299aab2104694dcf920c6
muhammadqamar <muhammadqamar111@gmail.com>
2020-07-28 19:33:15 +0500

    errora fixed

--- src/helpers/SharePreviewPopup.js
+++ src/helpers/SharePreviewPopup.js
@@ -1,30 +1,32 @@
-
-import React from 'react'
+import React from "react";
 import {
-    EmailShareButton,
-    FacebookShareButton,
-    TwitterShareButton,
-  } from "react-share";
-  import {
-    EmailIcon,
-    FacebookIcon,
-    TwitterIcon,
-  } from "react-share";
+  EmailShareButton,
+  FacebookShareButton,
+  TwitterShareButton,
+} from "react-share";
+import { EmailIcon, FacebookIcon, TwitterIcon } from "react-share";
 
-  import { confirmAlert } from 'react-confirm-alert'; // Import
-  import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
-
+import { confirmAlert } from "react-confirm-alert"; // Import
+import "react-confirm-alert/src/react-confirm-alert.css"; // Import css
 
 const SharePreviewPopup = (url, projectName) => {
-    confirmAlert({
-      customUI: ({ onClose }) => {
-        return (
-          <div className='share-project-preview-url'>
-            <h1>Your can now share project <strong>"{projectName}"</strong><br />
-                                Anyone with the link below can access your project:<br />
-        <br /><a target="_blank" href={url}>{url}</a><br /></h1>
-        <hr />
-        {/* <div className="margin-bottom-20">
+  confirmAlert({
+    customUI: ({ onClose }) => {
+      return (
+        <div className="share-project-preview-url project-share-check">
+          <h1>
+            Your can now share project <strong>"{projectName}"</strong>
+            <br />
+            Anyone with the link below can access your project:
+            <br />
+            <br />
+            <a target="_blank" href={url}>
+              {url}
+            </a>
+            <br />
+          </h1>
+          <hr />
+          {/* <div className="margin-bottom-20">
           <span>
             <FacebookShareButton url={url} className='share'>
               <FacebookIcon size={32} />
@@ -32,7 +34,7 @@ const SharePreviewPopup = (url, projectName) => {
           </span>
           <span className="margin-left-20 inline-block">Share this project on Facebook</span>
         </div> */}
-        {/* <div className="margin-bottom-20">
+          {/* <div className="margin-bottom-20">
           <span>
             <TwitterShareButton
                 url={url}
@@ -45,46 +47,45 @@ const SharePreviewPopup = (url, projectName) => {
           </span>
           <span className="margin-left-20 inline-block">Share this project on Twitter</span>
         </div> */}
-        <div className="margin-bottom-20">
-          <span>
-            <div id="croom">
-              <div class="g-sharetoclassroom" data-size="32" data-url={url} >Loading Classroom...</div>
-              <span className="margin-left-20 inline-block">Share this project on Google Classroom</span>
-            </div>
-          </span>
-          
-        </div>
-        <div className="margin-bottom-20">
-          <span>
-          <EmailShareButton
+          <div className="margin-bottom-20">
+            <span>
+              <div id="croom">
+                <div class="g-sharetoclassroom" data-size="32" data-url={url}>
+                  Loading Classroom...
+                </div>
+                <span className="margin-left-20 inline-block">
+                  Share this project on Google Classroom
+                </span>
+              </div>
+            </span>
+          </div>
+          <div className="margin-bottom-20">
+            <span>
+              <EmailShareButton
                 url={url}
                 subject={projectName}
-                className="email-share">
-                <EmailIcon
-                  size={32}
-                   />
+                className="email-share"
+              >
+                <EmailIcon size={32} />
               </EmailShareButton>
-          </span>
-          <span className="margin-left-20 inline-block">Share this project through email</span>
-        </div>
-  
-        
-  
-            <div className="close-btn">
-              <button onClick={onClose}>Ok</button>
-            </div>
-            <div className="google-script">
-            {
-              setTimeout(function(){
-                window.gapi.sharetoclassroom.go("croom")
-              }, 1)
-            }
-            
-            </div>
+            </span>
+            <span className="margin-left-20 inline-block">
+              Share this project through email
+            </span>
           </div>
-        );
-      }
-    });
-  };
 
-  export default SharePreviewPopup
+          <div className="close-btn">
+            <button onClick={onClose}>Ok</button>
+          </div>
+          <div className="google-script">
+            {setTimeout(function () {
+              window.gapi.sharetoclassroom.go("croom");
+            }, 1)}
+          </div>
+        </div>
+      );
+    },
+  });
+};
+
+export default SharePreviewPopup;
