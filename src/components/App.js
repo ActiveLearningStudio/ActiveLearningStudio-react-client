7f93de9f97f1d951deda33b3c4130e5bbe8009b7
muhammadqamar <muhammadqamar111@gmail.com>
2020-08-06 19:39:00 +0500

    env file addition

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,7 @@
 import React, { useEffect } from "react";
 
 import "./../styles/App.css";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "@apollo/react-hooks";
-import Home from "./Home";
+
 import "./../App.scss";
 import { Helmet } from "react-helmet";
 
