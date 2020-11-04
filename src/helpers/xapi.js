5f6cb6d5fa1d029d9c47e7c64ceef5cf8e9e4ae4
i-do-dev <i.do.dev@outlook.com>
2020-11-04 19:32:52 +0500

    TODOs for xapi enhancments

--- src/helpers/xapi.js
+++ src/helpers/xapi.js
@@ -12,7 +12,7 @@ export function H5PActvityPathMapToPlatform() {
   return [
     { '/project/:projectId/playlist/:playlistId/activity/:activityId/preview': 'CurrikiStudio' },
     { '/activity/:activityId/shared': 'CurrikiStudio' },
-    { '/gclass/launch/:userId/:courseId/:activityId/:classworkId': 'CurrikiStudio' },
+    { '/gclass/launch/:userId/:courseId/:activityId/:classworkId': 'Google Classroom' },
   ];
 }
 
@@ -29,13 +29,28 @@ export function extendStatement(statement, props) {
     statementExtended.context.platform = platformName;
   }
 
+  // TODO (1) - in new prevew we will not have 'parentPlaylist' in props so handle it accordingly. probably need to remove if statment
   if (props.parentPlaylist !== undefined) {
+    // TODO (2) - need to set ativityPreviewUrl constant on new pattren of http://localhost:3000/activity/18041/submission/Cg4I4uew5KIEEL_tuLHoBQ
+    // it hold the debate of getting submission id
     const ativityPreviewUrl = `${window.location.origin}/project/${props.parentPlaylist.project_id
     }/playlist/${props.parentPlaylist.id
     }/activity/${props.activityId}/preview`;
     const grouping = [{ objectType: 'Activity', id: ativityPreviewUrl }];
     statementExtended.context.contextActivities.grouping = grouping;
   }
+
+  // TODO (3) - update prop "statementExtended.actor" according to requirment mentioned below. This is xAPI statement 'Agent' property enhancement
+  // it holds the debate we have around getting gc-user-id
+  /*
+    {
+      "objectType": "Agent",
+      "account": {
+        "homePage": "https://classroom.google.com"
+        "name": "[gc-user-id]"
+      }
+    }
+  */
 
   return statementExtended;
 }
