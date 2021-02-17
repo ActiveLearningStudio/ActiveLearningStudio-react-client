cfc1c67ea7e113cf9134220267518dcb82cbf25f
Aftab <Muhammadqamar.Aftab@spglobal.com>
2021-02-18 00:03:29 +0500

    google class room changes

--- src/store/reducers/project.js
+++ src/store/reducers/project.js
@@ -9,7 +9,7 @@ const INITIAL_STATE = {
   projectSelect: {},
   lmsCourse: null,
   clone: [],
-  isSharedProject: false,
+  isSharedProject: null,
 };
 
 export default (state = INITIAL_STATE, action) => {
