ee05b37dd2df6b94a1c621b78e25ece778bd2ddd
Aftab <Muhammadqamar.Aftab@spglobal.com>
2021-03-23 21:21:15 +0500

    organization check

--- src/containers/ManageOrganization/intoOrganization.js
+++ src/containers/ManageOrganization/intoOrganization.js
@@ -17,7 +17,8 @@ export default function IntroOrganizations(props) {
   const dispatch = useDispatch();
   const { detail } = props;
   const history = useHistory();
-  const allState = useSelector((state) => state);
+  const allState = useSelector((state) => state.organization);
+  const { currentOrganization } = allState;
   // useMemo(() => {
   //   if() {
   //     dispatch(updatePreviousScreen(''));
@@ -47,104 +48,108 @@ export default function IntroOrganizations(props) {
               {detail.organization_role}
             </div>
           </div>
-          <div className="grp-btn">
-            <button className="sub-organization-button" type="button">
-              <div
-                className="button-text"
-                onClick={() => {
-                  dispatch(updateOrganizationScreen('create-org'));
-                }}
-              >
-                New Sub-organization
+          {currentOrganization?.organization_role_id === 1 && (
+            <div className="grp-btn">
+              <button className="sub-organization-button" type="button">
+                <div
+                  className="button-text"
+                  onClick={() => {
+                    dispatch(updateOrganizationScreen('create-org'));
+                  }}
+                >
+                  New Sub-organization
+                </div>
+              </button>
+              <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                  Invite Users
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <InviteOrganization />
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
+          )}
+        </div>
+        {currentOrganization?.organization_role_id === 1 && (
+          <div className="user-state-org">
+            <div>
+              <div className="circle">
+                <div className="count">{detail.suborganization_count || 0}</div>
+                <div className="value">Organization</div>
               </div>
-            </button>
-            <Dropdown>
-              <Dropdown.Toggle variant="success" id="dropdown-basic">
-                Invite Users
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                <InviteOrganization />
-              </Dropdown.Menu>
-            </Dropdown>
-          </div>
-        </div>
-        <div className="user-state-org">
-          <div>
-            <div className="circle">
-              <div className="count">{detail.suborganization_count || 0}</div>
-              <div className="value">Organization</div>
+              <div
+                onClick={() => {
+                  dispatch(updateOrganizationScreen('all-list'));
+                  dispatch(updatePreviousScreen('intro'));
+                  dispatch(clearSuborgList());
+                  dispatch(saveHistory(detail));
+                }}
+                className="more"
+              >
+                See More
+              </div>
             </div>
-            <div
-              onClick={() => {
-                dispatch(updateOrganizationScreen('all-list'));
-                dispatch(updatePreviousScreen('intro'));
-                dispatch(clearSuborgList());
-                dispatch(saveHistory(detail));
-              }}
-              className="more"
-            >
-              See More
+            <div>
+              <div className="circle">
+                <div className="count">{detail.users_count || 0}</div>
+                <div className="value">Users</div>
+              </div>
+              <div
+                onClick="#"
+                //   () => {
+                //   dispatch(updateOrganizationScreen('all-list'));
+                //   dispatch(updatePreviousScreen('projects'));
+                // }
+                className="more"
+              >
+                See More
+              </div>
             </div>
-          </div>
-          <div>
-            <div className="circle">
-              <div className="count">{detail.users_count || 0}</div>
-              <div className="value">Users</div>
+            <div>
+              <div className="circle">
+                <div className="count">{detail.groups_count || 0}</div>
+                <div className="value">Groups</div>
+              </div>
+              <div
+                onClick={() => {
+                  history.push(`/org/${currentOrganization?.domain}/groups`);
+                }}
+                className="more"
+              >
+                See More
+              </div>
             </div>
-            <div
-              onClick="#"
-              //   () => {
-              //   dispatch(updateOrganizationScreen('all-list'));
-              //   dispatch(updatePreviousScreen('projects'));
-              // }
-              className="more"
-            >
-              See More
+            <div>
+              <div className="circle">
+                <div className="count">{detail.teams_count || 0}</div>
+                <div className="value">Teams</div>
+              </div>
+              <div
+                onClick={() => {
+                  history.push(`/org/${currentOrganization?.domain}/teams`);
+                }}
+                className="more"
+              >
+                See More
+              </div>
             </div>
-          </div>
-          <div>
-            <div className="circle">
-              <div className="count">{detail.groups_count || 0}</div>
-              <div className="value">Groups</div>
-            </div>
-            <div
-              onClick={() => {
-                history.push(`/org/${allState.organization.currentOrganization?.domain}/groups`);
-              }}
-              className="more"
-            >
-              See More
-            </div>
-          </div>
-          <div>
-            <div className="circle">
-              <div className="count">{detail.teams_count || 0}</div>
-              <div className="value">Teams</div>
-            </div>
-            <div
-              onClick={() => {
-                history.push(`/org/${allState.organization.currentOrganization?.domain}/teams`);
-              }}
-              className="more"
-            >
-              See More
+            <div>
+              <div className="circle">
+                <div className="count">{detail.projects_count || 0}</div>
+                <div className="value">Projects</div>
+              </div>
+              <div
+                onClick={() => {
+                  history.push('/');
+                }}
+                className="more"
+              >
+                See More
+              </div>
             </div>
           </div>
-          <div>
-            <div className="circle">
-              <div className="count">{detail.projects_count || 0}</div>
-              <div className="value">Projects</div>
-            </div>
-            <div
-              onClick={() => {
-                history.push('/');
-              }}
-              className="more"
-            >
-              See More
-            </div>
-          </div>
-        </div>
+        )}
       </>
     ) : <Alert style={{ marginTop: '15px' }} variant="primary">Loading ...</Alert>
   );
