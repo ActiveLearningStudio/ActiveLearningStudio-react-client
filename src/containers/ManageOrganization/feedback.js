f6fecc3259b761a743183c8da8ddc49781c9a98f
Atabic <atabic14@gmail.com>
2021-04-01 17:23:07 +0500

    User First Level UI Fixes

--- src/containers/ManageOrganization/feedback.js
+++ src/containers/ManageOrganization/feedback.js
@@ -1,25 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { updateOrganizationScreen, clearHistory, setActiveOrganization } from 'store/actions/organization';
+import {
+  updateOrganizationScreen, clearHistory, setActiveOrganization, updatePreviousScreen,
+} from 'store/actions/organization';
 
 const Feedback = () => {
   const dispatch = useDispatch();
   const stateFeedback = useSelector((state) => state.organization);
-  const { newlyCreated, feedbackType, history } = stateFeedback;
+  const {
+    newlyCreated, feedbackType, history,
+  } = stateFeedback;
   return (
     <div className="feedback-org">
-      <h4>
-        Your organization&nbsp;
-        <strong>
-          {newlyCreated?.name}
-        </strong>
-        &nbsp;has been
-        &nbsp;
-        {feedbackType === 'create' && 'created!'}
-        {feedbackType === 'delete' && 'deleted!'}
-        {feedbackType === 'update' && 'updated'}
-      </h4>
+      {feedbackType?.action === 'user:delete'
+        ? (
+          <h4>
+            The user&nbsp;
+            <strong>
+              {feedbackType?.name}
+            </strong>
+            &nbsp;has been
+            deleted!
+          </h4>
+        )
+        : (
+          <h4>
+            Your organization&nbsp;
+            <strong>
+              {newlyCreated?.name}
+            </strong>
+            &nbsp;has been
+            &nbsp;
+            {feedbackType === 'create' && 'created!'}
+            {feedbackType === 'delete' && 'deleted!'}
+            {feedbackType === 'update' && 'updated'}
+          </h4>
+        )}
       {feedbackType === 'create' && (
         <div>
           <p className="email">
@@ -41,18 +58,31 @@ const Feedback = () => {
           </div>
         </div>
       )}
-      <div
-        className="back"
-        onClick={() => {
-          if (history) {
-            dispatch(setActiveOrganization(history));
-            dispatch(clearHistory());
-          }
-          dispatch(updateOrganizationScreen('all-list'));
-        }}
-      >
-        Back to Organizations
-      </div>
+      {feedbackType?.action !== 'user:delete' ? (
+        <div
+          className="back"
+          onClick={() => {
+            if (history) {
+              dispatch(setActiveOrganization(history));
+              dispatch(clearHistory());
+            }
+            dispatch(updateOrganizationScreen('all-list'));
+          }}
+        >
+          Back to Organizations
+        </div>
+      )
+        : (
+          <div
+            className="back"
+            onClick={() => {
+              dispatch(updateOrganizationScreen('Users'));
+              dispatch(updatePreviousScreen('intro'));
+            }}
+          >
+            Back to Users
+          </div>
+        )}
     </div>
   );
 };
