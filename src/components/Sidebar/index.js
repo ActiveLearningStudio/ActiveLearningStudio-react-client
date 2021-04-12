3aaaaf51f1061e44e82b305bc19444b4718d7c91
Atabic <atabic14@gmail.com>
2021-04-12 21:30:48 +0500

    Sidebar items completed

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,14 +1,23 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, {
+  // useCallback,
+  useEffect,
+  // useState
+} from 'react';
 import PropTypes from 'prop-types';
 import { Link, withRouter } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
+import { Accordion } from 'react-bootstrap';
 import foldericon from 'assets/images/sidebar/folder-icon.png';
 import teamicon from 'assets/images/sidebar/team-icon.png';
-import dashboardicon from 'assets/images/sidebar/dashboard-icon.png';
+// import toggleButton from 'assets/images/sidebar/toggle-button.png';
+import groupicon from 'assets/images/sidebar/group-icon.png';
+import usersidebaricon from 'assets/images/sidebar/user-sidebar-icon.png';
+// import dashboardicon from 'assets/images/sidebar/dashboard-icon.png';
 import backgroundimg from 'assets/images/sidebar/background.png';
-
+import menu from 'assets/images/menu_square.png';
+import dashboardChevron from 'assets/images/dashboard-chevron.png';
+import childOrganizationIcon from 'assets/images/child-organization-icon.png';
 import {
   allSidebarProjects,
 } from 'store/actions/project';
@@ -17,17 +26,20 @@ import './style.scss';
 
 const PROJECTS = 'projects';
 const CHANNEL = 'channel';
-const TEAM = 'team';
+// const TEAM = 'team';
 
 function Sidebar(props) {
-  const { history, location } = props;
+  const {
+    // history,
+    location,
+  } = props;
 
   const dispatch = useDispatch();
 
   const allState = useSelector((state) => state);
 
-  const [selectedTeam, setSelectedTeam] = useState(null);
-  const [selectedCategory, setSelectedCategory] = useState(null);
+  // const [selectedTeam, setSelectedTeam] = useState(null);
+  // const [selectedCategory, setSelectedCategory] = useState(null);
   const organization = useSelector((state) => state.organization);
   const { permission } = organization;
 
@@ -35,14 +47,14 @@ function Sidebar(props) {
     if (location.pathname.includes('teams/')) {
       const teamId = parseInt(location.pathname.split('teams/')[1], 10);
       if (teamId) {
-        setSelectedTeam(teamId);
+        // setSelectedTeam(teamId);
 
         if (location.pathname.includes(PROJECTS)) {
-          setSelectedCategory(PROJECTS);
+          // setSelectedCategory(PROJECTS);
         } else if (location.pathname.includes(CHANNEL)) {
-          setSelectedCategory(CHANNEL);
+          // setSelectedCategory(CHANNEL);
         } else {
-          setSelectedCategory(TEAM);
+          // setSelectedCategory(TEAM);
         }
       }
     }
@@ -54,16 +66,106 @@ function Sidebar(props) {
     }
   }, [allState.sidebar.isLoaded, organization.activeOrganization, dispatch]);
 
-  const handleClickTeam = useCallback((team) => {
-    history.push(`/org/${allState.organization.currentOrganization?.domain}/teams/${team.id}`);
-  }, [history, allState.organization.currentOrganization?.domain]);
-  const handleClickGroup = useCallback((group) => {
-    history.push(`/org/${allState.organization.currentOrganization?.domain}/groups/${group.id}`);
-  }, [history, allState.organization.currentOrganization?.domain]);
+  // const handleClickTeam = useCallback((team) => {
+  //   history.push(`/org/${allState.organization.currentOrganization?.domain}/teams/${team.id}`);
+  // }, [history, allState.organization.currentOrganization?.domain]);
+  // const handleClickGroup = useCallback((group) => {
+  //   history.push(`/org/${allState.organization.currentOrganization?.domain}/groups/${group.id}`);
+  // }, [history, allState.organization.currentOrganization?.domain]);
 
   return (
     <aside className="sidebar-all">
-      <Link to="/">
+      <Accordion>
+        <Accordion.Toggle>
+          {/* <img src={toggleButton} alt="" /> */}
+        </Accordion.Toggle>
+      </Accordion>
+      {permission?.activeRole === 'admin'
+      && (
+        <>
+          <div className="row-org">
+            <img src={menu} alt="menu_square" className="org-icon" />
+            <div className="org-heading">
+              Organization
+            </div>
+          </div>
+          <div className="org-name">
+            {allState.organization.currentOrganization?.name}
+          </div>
+          <Link to={`/org/${allState.organization.currentOrganization?.domain}/manage`}>
+            <div className="goto-dashboard">
+              Go to Dashboard
+              <img src={dashboardChevron} alt="" className="dashboard-chevron" />
+            </div>
+          </Link>
+          <Link to={`/org/${allState.organization.currentOrganization?.domain}/manage`}>
+            <div className="row-sidebar">
+              <img src={childOrganizationIcon} alt="" />
+              <div className="sidebar-headings">
+                Child Orgs
+              </div>
+              <FontAwesomeIcon icon="angle-right" className="headings-angle-right" />
+            </div>
+          </Link>
+        </>
+      )}
+      {permission?.Project
+      && (
+        <>
+          <Link to={`/org/${allState.organization.currentOrganization?.domain}`}>
+            <div className="row-sidebar">
+              <img src={foldericon} alt="" />
+              <div className="sidebar-headings">
+                Projects
+              </div>
+              <FontAwesomeIcon icon="angle-right" className="headings-angle-right" />
+            </div>
+          </Link>
+        </>
+      )}
+      {permission?.Team
+      && (
+        <>
+          <Link to={`/org/${allState.organization.currentOrganization?.domain}/teams`}>
+            <div className="row-sidebar">
+              <img src={teamicon} alt="" />
+              <div className="sidebar-headings">
+                Teams
+              </div>
+              <FontAwesomeIcon icon="angle-right" className="headings-angle-right" />
+            </div>
+          </Link>
+        </>
+      )}
+      {permission?.Group
+      && (
+        <>
+          <Link to={`/org/${allState.organization.currentOrganization?.domain}/groups`}>
+            <div className="row-sidebar">
+              <img src={groupicon} alt="" />
+              <div className="sidebar-headings">
+                Groups
+              </div>
+              <FontAwesomeIcon icon="angle-right" className="headings-angle-right" />
+            </div>
+          </Link>
+        </>
+      )}
+      {permission?.Group
+      && (
+        <>
+          <Link to={`/org/${allState.organization.currentOrganization?.domain}/manage`}>
+            <div className="row-sidebar">
+              <img src={usersidebaricon} alt="" />
+              <div className="sidebar-headings">
+                Users
+              </div>
+              <FontAwesomeIcon icon="angle-right" className="headings-angle-right" />
+            </div>
+          </Link>
+        </>
+      )}
+      {/* <Link to="/">
         <div className="menu-title">
           <img src={foldericon} alt="" />
           My Projects
@@ -92,8 +194,8 @@ function Sidebar(props) {
             Dashboard & Stats
           </div>
         </Link>
-      )}
-      {permission?.Team?.includes('team:view') && (
+      )} */}
+      {/* {permission?.Team?.includes('team:view') && (
         <>
           <Link to={`/org/${allState.organization.currentOrganization?.domain}/teams`}>
             <div className="menu-title">
@@ -125,14 +227,12 @@ function Sidebar(props) {
                 >
                   <span className="project-title">Projects</span>
                 </Link>
-                {/*
                 <Link
                   to={`/teams/${team.id}/channel`}
                   className={selectedCategory === CHANNEL ? 'active-label' : ''}
                 >
                   <span className="channel-title">Channels</span>
                 </Link>
-                */}
               </div>
             </div>
           ))}
@@ -144,8 +244,8 @@ function Sidebar(props) {
             </Link>
             )}
         </>
-      )}
-      {permission?.Team?.includes('team:create') && (
+      )} */}
+      {/* {permission?.Team?.includes('team:create') && (
         <div className="menu-title create-button">
           <Link to={`/org/${allState.organization.currentOrganization?.domain}/teams/create-team`}>
             <div>
@@ -154,8 +254,8 @@ function Sidebar(props) {
             </div>
           </Link>
         </div>
-      )}
-      {permission?.Group?.includes('group:view') && (
+      )} */}
+      {/* {permission?.Group?.includes('group:view') && (
         <>
           <Link to={`/org/${allState.organization.currentOrganization?.domain}/groups`}>
             <div className="menu-title">
@@ -187,14 +287,12 @@ function Sidebar(props) {
                 >
                   <span className="project-title">Projects</span>
                 </Link>
-                {/*
                 <Link
                   to={`/teams/${team.id}/channel`}
                   className={selectedCategory === CHANNEL ? 'active-label' : ''}
                 >
                   <span className="channel-title">Channels</span>
                 </Link>
-                */}
               </div>
             </div>
           ))}
@@ -206,8 +304,8 @@ function Sidebar(props) {
             </Link>
             )}
         </>
-      )}
-      {permission?.Group?.includes('group:create') && (
+      )} */}
+      {/* {permission?.Group?.includes('group:create') && (
         <div className="menu-title create-button">
           <Link to={`/org/${allState.organization.currentOrganization?.domain}/groups/create-group`}>
             <div>
@@ -216,14 +314,14 @@ function Sidebar(props) {
             </div>
           </Link>
         </div>
-      )}
+      )} */}
       <img src={backgroundimg} alt="" />
     </aside>
   );
 }
 
 Sidebar.propTypes = {
-  history: PropTypes.object.isRequired,
+  // history: PropTypes.object.isRequired,
   location: PropTypes.object.isRequired,
 };
 
