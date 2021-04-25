a885a8b9d1653c17761edea10c6351649518cf43
Ali Mehdi <imcoder001@gmail.com>
2021-04-25 18:21:54 +0500

    Update entrypoint.client.sh

--- entrypoint.client.sh
+++ entrypoint.client.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-npm run build && mkdir -p /app/html && cp -rf /app/build/* /app/html &
-npm run start:prod
+if [ ! -e /app/health.ok ]; then touch /app/health.ok && npm run build && mkdir -p /app/html && cp -rf /app/build/* /app/html; fi
 
+npm run start:prod
