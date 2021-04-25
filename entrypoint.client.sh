ce7e7bf4030d66a7d33f628732a257ef2b94ac19
IMCoder <imcoder001@gmail.com>
2021-04-25 16:54:46 +0500

    client entrypoint

--- entrypoint.client.sh
+++ entrypoint.client.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if [ ! -e /app/buildhasrun ]
-    npm run build && mkdir -p /app/html && cp -rf /app/build/* /app/html &
-    touch /app/buildhasrun
-fi
-
+npm run build && mkdir -p /app/html && cp -rf /app/build/* /app/html &
 npm run start:prod
 
+# if [ ! -e /app/buildhasrun ]
+#     npm run build && mkdir -p /app/html && cp -rf /app/build/* /app/html &
+#     touch /app/buildhasrun
+# fi
+
+# npm run start:prod
