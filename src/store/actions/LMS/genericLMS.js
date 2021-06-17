371e94b851e9fabacf8919efe142f76471548dd4
Antonio Etayo <antonioetayo@gmail.com>
2021-06-17 11:19:24 -0400

    Implemented frontend changes for safari montage integration

--- src/store/actions/LMS/genericLMS.js
+++ src/store/actions/LMS/genericLMS.js
@@ -4,6 +4,9 @@ import {
   GENERIC_LMS_LOGIN_ERROR,
   GENERIC_LMS_LOAD_H5P_SETTINGS,
   GENERIC_LMS_LOAD_H5P_SETTINGS_ERROR,
+  SAFARI_MONTAGE_LOAD_PUBLISH_TOOL,
+  SAFARI_MONTAGE_LOAD_PUBLISH_TOOL_ERROR,
+  CLOSE_SAFARI_MONTAGE_TOOL,
 } from '../../genericLMSActionTypes';
 
 export const doLoginAction = (params) => async (dispatch) => {
@@ -34,4 +37,23 @@ export const loadH5PSettingsAction = (activityId) => async (dispatch) => {
       results,
     });
   }
+};
+
+export const loadSafariMontagePublishToolAction = (projectId, playlistId, activityId, lmsSettingId) => async (dispatch) => {
+  const results = await service.loadSafariMontagePublishTool(projectId, playlistId, activityId, lmsSettingId);
+  if (results.errors) {
+    dispatch({
+      type: SAFARI_MONTAGE_LOAD_PUBLISH_TOOL_ERROR,
+      results,
+    });
+  } else {
+    dispatch({
+      type: SAFARI_MONTAGE_LOAD_PUBLISH_TOOL,
+      results,
+    });
+  }
+};
+
+export const closeSafariMontageToolAction = () => async (dispatch) => {
+  dispatch({ type: CLOSE_SAFARI_MONTAGE_TOOL });
 };
