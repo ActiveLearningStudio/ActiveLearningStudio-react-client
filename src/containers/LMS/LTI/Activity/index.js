fb9817a25cc28cc4ab854401f630e6cde4968fe7
Muhammad <muhammadqamar111@gmail.com>
2021-08-04 20:38:07 +0500

    size changes

--- src/containers/LMS/LTI/Activity/index.js
+++ src/containers/LMS/LTI/Activity/index.js
@@ -171,7 +171,7 @@ const Activity = (props) => {
       )}
 
       {!ltiFinished && (
-        <div id="curriki-h5p-wrapper" style={{ maxWidth: '75%', margin: '0 auto' }}>
+        <div id="curriki-h5p-wrapper" style={{ maxWidth: '65%', margin: '0 auto' }}>
           <div className="loader_gif">
             <img style={{ width: '50px' }} src={gifloader} alt="" />
           </div>
