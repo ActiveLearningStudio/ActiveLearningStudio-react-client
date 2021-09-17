cd7b7a6840b964deb9cadd747b64923b8ead06c0
Atabic <atabic14@gmail.com>
2021-09-17 20:29:46 +0500

    New permissions added

--- src/containers/Teams/TeamProjectView/index.js
+++ src/containers/Teams/TeamProjectView/index.js
@@ -31,7 +31,7 @@ function TeamProjectView(props) {
   const { teamPermission, selectedForClone } = useSelector((state) => state.team);
   const { notification } = useSelector((state) => state.notification);
   const dispatch = useDispatch();
-  const { permission } = organization;
+  // const { permission } = organization;
   const [selectedProjectId, setSelectedProjectId] = useState(0);
   const [show, setShow] = useState(false);
   const authUser = users.find((u) => u.id === (user || {}).id);
@@ -113,19 +113,19 @@ function TeamProjectView(props) {
                   </Dropdown.Toggle>
 
                   <Dropdown.Menu>
-                    {permission?.Project?.includes('project:view') && (
+                    {teamPermission?.Team?.includes('team:view-project') && (
                       <Dropdown.Item as={Link} to={`/org/${organization.currentOrganization?.domain}/project/${project.id}/preview`}>
                         <FontAwesomeIcon icon="eye" className="mr-2" />
                         Preview
                       </Dropdown.Item>
                     )}
-                    {permission?.Project?.includes('project:view') && (
+                    {teamPermission?.Team?.includes('team:view-project') && (
                       <Dropdown.Item as={Link} to={`/org/${organization.currentOrganization?.domain}/project/${project.id}`}>
                         <FontAwesomeIcon icon="globe" className="mr-2" />
                         Build
                       </Dropdown.Item>
                     )}
-                    {permission?.Project?.includes('project:publish') && (
+                    {teamPermission?.Team?.includes('team:publish-project') && (
                       <li className="dropdown-submenu send">
                         <a tabIndex="-1">
                           <FontAwesomeIcon icon="newspaper" className="mr-2" />
@@ -189,7 +189,7 @@ function TeamProjectView(props) {
                         Share
                       </Dropdown.Item>
                     )}
-                    {permission?.Project?.includes('project:edit') && (
+                    {teamPermission?.Team?.includes('team:edit-project') && (
                       <Dropdown.Item as={Link} to={`/org/${organization.currentOrganization?.domain}/project/${project.id}/edit`}>
                         <FontAwesomeIcon icon="pen" className="mr-2" />
                         Edit
