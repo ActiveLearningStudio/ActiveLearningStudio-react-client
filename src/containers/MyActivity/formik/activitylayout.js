b0560df86ec17d0b8c5ee6a10df410502e93867b
Muhammad <muhammadqamar111@gmail.com>
2021-10-11 16:10:34 +0500

    activiy upload things

--- src/containers/MyActivity/formik/activitylayout.js
+++ src/containers/MyActivity/formik/activitylayout.js
@@ -1,37 +1,37 @@
 /* eslint-disable */
-import React, { useState, useEffect, useMemo } from "react";
-import HeadingText from "utils/HeadingText/headingtext";
-import HeadingTwo from "utils/HeadingTwo/headingtwo";
-import LayoutCard from "utils/LayoutCard/layoutcard";
-import { useSelector, useDispatch } from "react-redux";
-import { toast } from "react-toastify";
-import ColoumImage1 from "assets/images/layout/singleactivit.png";
-import Tabs from "utils/Tabs/tabs";
-import Buttons from "utils/Buttons/buttons";
-import HeadingThree from "utils/HeadingThree/headingthree";
-import PlayIcon from "assets/images/play.png";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import { useHistory } from "react-router-dom";
-import { getLayoutActivities } from "store/actions/resource";
-import * as actionTypes from "store/actionTypes";
-import loader from "assets/images/loader.svg";
+import React, { useState, useEffect, useMemo } from 'react';
+import HeadingText from 'utils/HeadingText/headingtext';
+import HeadingTwo from 'utils/HeadingTwo/headingtwo';
+import LayoutCard from 'utils/LayoutCard/layoutcard';
+import { useSelector, useDispatch } from 'react-redux';
+import { toast } from 'react-toastify';
+import ColoumImage1 from 'assets/images/layout/singleactivit.png';
+import Tabs from 'utils/Tabs/tabs';
+import Buttons from 'utils/Buttons/buttons';
+import HeadingThree from 'utils/HeadingThree/headingthree';
+import PlayIcon from 'assets/images/play.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { useHistory } from 'react-router-dom';
+import { getLayoutActivities } from 'store/actions/resource';
+import * as actionTypes from 'store/actionTypes';
+import loader from 'assets/images/loader.svg';
 
-const ImgLoader = () => <img style={{ width: "100px" }} src={loader} />;
+const ImgLoader = () => <img style={{ width: '100px' }} src={loader} />;
 const ActivityLayout = (props) => {
   const { changeScreenHandler } = props;
   const history = useHistory();
-  const [layout, setLayout] = useState({ title: "Interactive Book" });
+  const [layout, setLayout] = useState({ title: 'Interactive Book' });
   const dispatch = useDispatch();
 
   useMemo(() => {
-    toast.info("Loading Activities ...", {
-      className: "project-loading",
+    toast.info('Loading Activities ...', {
+      className: 'project-loading',
       closeOnClick: false,
       closeButton: false,
       position: toast.POSITION.BOTTOM_RIGHT,
       autoClose: 10000,
-      icon: "",
+      icon: '',
     });
     dispatch(getLayoutActivities());
   }, []);
@@ -54,24 +54,24 @@ const ActivityLayout = (props) => {
         <HeadingTwo text="Select a layout" color="#084892" />
       </div>
       <div className="activity-layout-detail">
-        <HeadingText
-          text="Start creating by selecting a layout and then add activity types."
-          color="#515151"
-        />
+        <HeadingText text="Start creating by selecting a layout and then add activity types." color="#515151" />
       </div>
       <div className="layout-cards-process-btn">
         <div className="activity-layout-cards">
+          <div
+            onClick={() => {
+              changeScreenHandler('addactivity', 'upload');
+            }}
+          >
+            test
+          </div>
           {!!allActivity &&
             allActivity.map((data) => {
               return (
                 <LayoutCard
                   image={data.image}
                   text={data.title}
-                  className={
-                    layout?.title == data.title
-                      ? "activity-layoutCard-active mr-3"
-                      : "mr-3"
-                  }
+                  className={layout?.title == data.title ? 'activity-layoutCard-active mr-3' : 'mr-3'}
                   onClick={() => setLayout(data)}
                 />
               );
@@ -82,16 +82,12 @@ const ActivityLayout = (props) => {
               image={ColoumImage1}
               text="Single Activity"
               importImg
-              className={
-                layout == "SingleActivity"
-                  ? "activity-layoutCard-active mr-30"
-                  : "mr-30"
-              }
+              className={layout == 'SingleActivity' ? 'activity-layoutCard-active mr-30' : 'mr-30'}
               onClick={() => {
-                setLayout("SingleActivity");
+                setLayout('SingleActivity');
                 dispatch({
                   type: actionTypes.SET_ACTIVE_ACTIVITY_SCREEN,
-                  payload: "singleActivity",
+                  payload: 'singleActivity',
                 });
               }}
             />
@@ -101,16 +97,7 @@ const ActivityLayout = (props) => {
           <div className="layout-process-btn">
             <HeadingThree text={layout.title} color="#084892" />
             <div className="activity-layout-process-box">
-              <iframe
-                width="100%"
-                height="100%"
-                frameborder="0"
-                src={
-                  layout.demo_video_id ||
-                  "https://www.youtube.com/embed/ngXSzWNYzU4"
-                }
-                title={layout.title}
-              ></iframe>
+              <iframe width="100%" height="100%" frameborder="0" src={layout.demo_video_id || 'https://www.youtube.com/embed/ngXSzWNYzU4'} title={layout.title}></iframe>
               {/* <img src={PlayIcon} /> */}
             </div>
             <HeadingText text={layout.description} color="#515151" />
@@ -119,20 +106,10 @@ const ActivityLayout = (props) => {
                 <HeadingThree text="Useful for" color="#084892" />
                 <FontAwesomeIcon icon={faArrowRight} className="useful-icon" />
               </div>
-              <HeadingText
-                text="Guided onboardings, Corporated presentations"
-                color="#515151"
-              />
+              <HeadingText text="Guided onboardings, Corporated presentations" color="#515151" />
             </div>
             <div className="activity-layout-btns">
-              <Buttons
-                text="Cancel"
-                secondary={true}
-                width="153px"
-                height="36px"
-                onClick={() => changeScreenHandler("")}
-                hover={true}
-              />
+              <Buttons text="Cancel" secondary={true} width="153px" height="36px" onClick={() => changeScreenHandler('')} hover={true} />
 
               <div className="btns-margin">
                 <Buttons
@@ -142,7 +119,7 @@ const ActivityLayout = (props) => {
                   height="36px"
                   disabled={layout ? false : true}
                   onClick={() => {
-                    changeScreenHandler("addactivity");
+                    changeScreenHandler('addactivity');
                     dispatch({
                       type: actionTypes.SET_SELECTED_ACTIVITY,
                       payload: layout,
--- src/containers/MyActivity/index.js
+++ src/containers/MyActivity/index.js
@@ -1,38 +1,27 @@
 /*eslint-disable*/
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router";
-import TopHeading from "utils/TopHeading/topheading";
-import TopHeadingImage from "assets/images/svg/myProject.svg";
-import "./styles.scss";
-import Buttons from "utils/Buttons/buttons";
-import * as actionTypes from "store/actionTypes";
-import { useSelector, useDispatch } from "react-redux";
-import {
-  faBoxTissue,
-  faList,
-  faListAlt,
-  faPlus,
-  faTh,
-} from "@fortawesome/free-solid-svg-icons";
-import HeadingText from "utils/HeadingText/headingtext";
-import HeadingTwo from "utils/HeadingTwo/headingtwo";
-import HeadingThree from "utils/HeadingThree/headingthree";
-import NewActivity from "./formik/newactivity";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import ActivityLayout from "./formik/activitylayout";
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router';
+import TopHeading from 'utils/TopHeading/topheading';
+import TopHeadingImage from 'assets/images/svg/myProject.svg';
+import './styles.scss';
+import Buttons from 'utils/Buttons/buttons';
+import * as actionTypes from 'store/actionTypes';
+import { useSelector, useDispatch } from 'react-redux';
+import { faBoxTissue, faList, faListAlt, faPlus, faTh } from '@fortawesome/free-solid-svg-icons';
+import HeadingText from 'utils/HeadingText/headingtext';
+import HeadingTwo from 'utils/HeadingTwo/headingtwo';
+import HeadingThree from 'utils/HeadingThree/headingthree';
+import NewActivity from './formik/newactivity';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import ActivityLayout from './formik/activitylayout';
 import SingleActivity from './formik/addSingleActivity';
-import AddActivity from "./formik/addactivity";
-import PreviewLayout from "./formik/previewlayout";
-import { Link } from "react-router-dom";
-import { Formik, Field } from "formik";
-import CardImage from "assets/images/activitycard.png";
-import ActivityCardBox from "utils/ActivityCard/activitycard";
-import UploadInteractiveVideo from "./formik/uploadinteractivevideo";
-import ActivityCardV2 from "utils/ActivityCardV2/activitycardv2";
-import Footer from "components/Footer";
-import AddActivityPopup from "utils/AddActivityPopup/addactivitypopup";
+import AddActivity from './formik/addactivity';
+import PreviewLayout from './formik/previewlayout';
+
+import UploadInteractiveVideo from './formik/uploadinteractivevideo';
+
 // import H5PEditor from "components/ResourceCard/AddResource/Editors/H5PEditorV2";
-const MyActivity = ({playlistPreview}) => {
+const MyActivity = ({ playlistPreview }) => {
   const [edit, setEdit] = useState(false);
   const [addActivityPopUp, setAddActivityPopUp] = useState(false);
   const params = useParams();
@@ -45,33 +34,29 @@ const MyActivity = ({playlistPreview}) => {
   const [uploadImageStatus, setUploadImageStatus] = useState(false);
   const [showFilter, setShowFilter] = useState(false);
   const [listView, setListView] = useState(false);
+  const [activtyMethod, setActivityMethod] = useState('create');
 
   const { screenState } = useSelector((state) => state.myactivities);
   const dispatch = useDispatch();
-  const changeScreenHandler = (payload) => {
+  const changeScreenHandler = (payload, method) => {
     dispatch({
       type: actionTypes.SET_ACTIVE_ACTIVITY_SCREEN,
       payload: payload,
     });
+    if (method === 'upload') {
+      setActivityMethod('upload');
+    } else {
+      setActivityMethod('create');
+    }
   };
   return (
     <>
       {screenState && (
-        <div
-          className={
-            uploadImageStatus
-              ? "form-new-popup-activity z-index"
-              : "form-new-popup-activity"
-          }
-        >
+        <div className={uploadImageStatus ? 'form-new-popup-activity z-index' : 'form-new-popup-activity'}>
           <div className="inner-form-content">
             <div className="inner-form-content-box">
               <div className="cross-all-pop-box">
-                <FontAwesomeIcon
-                  icon="times"
-                  className="cross-all-pop"
-                  onClick={() => changeScreenHandler("")}
-                />
+                <FontAwesomeIcon icon="times" className="cross-all-pop" onClick={() => changeScreenHandler('')} />
               </div>
 
               {/* {screenState === "newactivity" && (
@@ -80,284 +65,40 @@ const MyActivity = ({playlistPreview}) => {
                 screenState={screenState}
               />
             )} */}
-              {screenState === "layout" && (
-                <ActivityLayout
-                  changeScreenHandler={changeScreenHandler}
-                  screenState={screenState}
-                />
+              {screenState === 'layout' && <ActivityLayout changeScreenHandler={changeScreenHandler} screenState={screenState} />}
+              {screenState === 'addactivity' && (
+                <AddActivity changeScreenHandler={changeScreenHandler} screenState={screenState} activtyMethod={activtyMethod} setUploadImageStatus={setUploadImageStatus} />
               )}
-              {screenState === "addactivity" && (
-                <AddActivity
-                  changeScreenHandler={changeScreenHandler}
-                  screenState={screenState}
-                  setUploadImageStatus={setUploadImageStatus}
-                />
-              )}
-              {screenState === "uploadinteractivevideo" && (
-                <UploadInteractiveVideo
-                  changeScreenHandler={changeScreenHandler}
-                  screenState={screenState}
-                />
-              )}
-              {screenState === "preview" && (
-                <PreviewLayout
-                  changeScreenHandler={changeScreenHandler}
-                  screenState={screenState}
-              />
-              )}
-               {screenState === "singleActivity" && (
-                <SingleActivity
-                  changeScreenHandler={changeScreenHandler}
-                  screenState={screenState}
-              />
-              )}
+              {screenState === 'uploadinteractivevideo' && <UploadInteractiveVideo changeScreenHandler={changeScreenHandler} screenState={screenState} />}
+              {screenState === 'preview' && <PreviewLayout changeScreenHandler={changeScreenHandler} screenState={screenState} />}
+              {screenState === 'singleActivity' && <SingleActivity changeScreenHandler={changeScreenHandler} screenState={screenState} />}
             </div>
           </div>
         </div>
       )}
-      {!playlistPreview 
-      && (
-      <div className="myactivity">
-        <div className="content-wrapper">
-          <div className="inner-content">
-            {/* <H5PEditor /> */}
-            <div className="topHeadingBtn">
-              <div className="topHeading">
-                <TopHeading
-                  description="Nevada Department of Education"
-                  image={TopHeadingImage}
-                  heading="My Activities"
-                  color="#084892"
-                />
+      {!playlistPreview && (
+        <div className="myactivity">
+          <div className="content-wrapper">
+            <div className="inner-content">
+              <div className="topHeadingBtn">
+                <div className="topHeading">
+                  <TopHeading description="Nevada Department of Education" image={TopHeadingImage} heading="My Activities" color="#084892" />
+                </div>
+                <div className="topBtn">
+                  <Buttons primary={true} text="New Activity" icon={faPlus} width="163px" height="35px" onClick={() => changeScreenHandler('layout')} hover={true} />
+                </div>
               </div>
-              <div className="topBtn">
-                <Buttons
-                  primary={true}
-                  text="New Activity"
-                  icon={faPlus}
-                  width="163px"
-                  height="35px"
-                  onClick={() => changeScreenHandler("layout")}
-                  hover={true}
-                />
+              <div className="topDescription">
+                <HeadingText text="Create new activities, control, manage and organize them in playlists and projects." color="#515151" />
               </div>
-            </div>
-            <div className="topDescription">
-              <HeadingText
-                text="Create new activities, control, manage and organize them in playlists and projects."
-                color="#515151"
-              />
-            </div>
-            <div className="activityTitle">
-              {edit ? (
-                <HeadingTwo
-                  text="Select Activies to add to a project or create a new one."
-                  color="#285AA5"
-                />
-              ) : (
-                <HeadingTwo text="Activities" color="#285AA5" />
-              )}
-            </div>
+              <div className="activityTitle">
+                {edit ? <HeadingTwo text="Select Activies to add to a project or create a new one." color="#285AA5" /> : <HeadingTwo text="Activities" color="#285AA5" />}
+              </div>
 
-            {cardShow ? (
-              <div className="activity-cards-show">
-                <div className="activity-cards-search">
-                  <div className="search-textfield">
-                    <input type="text" placeholder="Search activity..." />
-                    <FontAwesomeIcon icon="search" />
-                  </div>
-                  <div className="search-filter">
-                    {/* <Link to="#" className="primary-color search-filter-link " >
-                      Sort & Filter
-                    </Link> */}
-                    <span
-                      className="primary-color search-filter-link"
-                      onClick={() => setShowFilter(!showFilter)}
-                    >
-                      Sort & Filter
-                    </span>
-                  </div>
-                  <div className="create-project-portion">
-                    {/* <Link to="#" className="primary-color">
-                      <FontAwesomeIcon
-                        icon="plus-square"
-                        className="project-portion-icon"
-                      />
-                      Create project
-                    </Link> */}
-                    <FontAwesomeIcon
-                      icon={faList}
-                      className="project-portion-icon primary-color"
-                      onClick={() => setListView(true)}
-                    />
-                    <FontAwesomeIcon
-                      icon={faTh}
-                      className="project-portion-icon primary-color"
-                      onClick={() => setListView(false)}
-                    />
-                  </div>
-                </div>
-                {showFilter && (
-                  <div className="search-filter-form">
-                    <Formik
-                      initialValues={{
-                        method: "",
-                        layoutTitle: "",
-                        picked: "create",
-                      }}
-                    >
-                      <form>
-                        <div className="sort-filter-form">
-                          <div className="sortby">
-                            <label>Sort by</label>
-                            <select>
-                              <option>A to Z</option>
-                              <option>A to Z</option>
-                              <option>A to Z</option>
-                            </select>
-                          </div>
-                          <div className="sortby">
-                            <label>Subject</label>
-                            <select>
-                              <option>Analisis</option>
-                              <option>Analisis</option>
-                              <option>Analisis</option>
-                            </select>
-                          </div>
-                          <div className="sortby">
-                            <label>Best for</label>
-                            <select>
-                              <option>Analisis</option>
-                              <option>Analisis</option>
-                              <option>Analisis</option>
-                            </select>
-                          </div>
-                        </div>
-                        <div className="filter-form-radio">
-                          <label className="check-input">
-                            <Field type="radio" name="sort" value="sort" />
-                            Image based
-                          </label>
-                          <label className="check-input ml-35">
-                            <Field type="radio" name="sort" value="sort" />
-                            Accesible
-                          </label>
-                          <label className="check-input ml-35">
-                            <Field type="radio" name="sort" value="sort" />
-                            Multimedia
-                          </label>
-                        </div>
-                      </form>
-                    </Formik>
-                  </div>
-                )}
-                {/* <div className="activity-cards-box">
-                  <div className={listView ? "width-100" : "ml-20"}>
-                    <ActivityCardBox
-                      img={CardImage}
-                      title="My first activity"
-                      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor."
-                      listView={listView}
-                    />
-                  </div>
-                  <div className={listView ? "width-100" : "ml-20"}>
-                    <ActivityCardBox
-                      img={CardImage}
-                      title="My first activity"
-                      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor."
-                      listView={listView}
-                    />
-                  </div>
-                  <div className={listView ? "width-100" : "ml-20"}>
-                    <ActivityCardBox
-                      img={CardImage}
-                      title="My first activity"
-                      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor."
-                      listView={listView}
-                    />
-                  </div>
-                  <div className={listView ? "width-100" : "ml-20"}>
-                    <ActivityCardBox
-                      img={CardImage}
-                      title="My first activity"
-                      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor."
-                      listView={listView}
-                    />
-                  </div>
-                  <div className={listView ? "width-100" : "ml-20"}>
-                    <ActivityCardBox
-                      img={CardImage}
-                      title="My first activity"
-                      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor."
-                      listView={listView}
-                    />
-                  </div>
-                  <div className={listView ? "width-100" : "ml-20"}>
-                    <ActivityCardBox
-                      img={CardImage}
-                      title="My first activity"
-                      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor."
-                      listView={listView}
-                    />
-                  </div>
-                </div> */}
-                <div className="activity-cards-box">
-                  <div className={listView ? "width-100" : "mt-36"}>
-                    <ActivityCardV2
-                      backgroundImg={CardImage}
-                      title="My first activity"
-                      listView={listView}
-                      selectionStatus={edit}
-                      setAddActivityPopUp={setAddActivityPopUp}
-                    />
-                  </div>
-                  <div className={listView ? "width-100" : "mt-36"}>
-                    <ActivityCardV2
-                      backgroundImg={CardImage}
-                      title="My first activity"
-                      listView={listView}
-                      selectionStatus={edit}
-                      setAddActivityPopUp={setAddActivityPopUp}
-                    />
-                  </div>
-                  <div className={listView ? "width-100" : "mt-36"}>
-                    <ActivityCardV2
-                      backgroundImg={CardImage}
-                      title="My first activity"
-                      listView={listView}
-                      selectionStatus={edit}
-                      setAddActivityPopUp={setAddActivityPopUp}
-                    />
-                  </div>
-                  <div className={listView ? "width-100" : "mt-36"}>
-                    <ActivityCardV2
-                      backgroundImg={CardImage}
-                      title="My first activity"
-                      listView={listView}
-                      selectionStatus={edit}
-                      setAddActivityPopUp={setAddActivityPopUp}
-                    />
-                  </div>
-                  <div className={listView ? "width-100" : "mt-36"}>
-                    <ActivityCardV2
-                      backgroundImg={CardImage}
-                      title="My first activity"
-                      listView={listView}
-                      selectionStatus={edit}
-                      setAddActivityPopUp={setAddActivityPopUp}
-                    />
-                  </div>
-                </div>
-              </div>
-            ) : (
               <div className="activityBlocks">
                 <div className="blockBody">
                   <div className="blockBody-detail">
-                    <HeadingThree
-                      text="How to create Activities"
-                      color="#515151"
-                      className="mb-20"
-                    />
+                    <HeadingThree text="How to create Activities" color="#515151" className="mb-20" />
                     <HeadingText
                       text="Learn how to create awesome 
                     activities using +50 Activty Types
@@ -372,18 +113,13 @@ const MyActivity = ({playlistPreview}) => {
 
                 <div className="blockBody ml-54">
                   <div className="blockBody-text">
-                    <HeadingText
-                      text="How to organize Activities into Projects"
-                      color="#515151"
-                      className=""
-                    />
+                    <HeadingText text="How to organize Activities into Projects" color="#515151" className="" />
                   </div>
                 </div>
               </div>
-            )}
+            </div>
           </div>
         </div>
-      </div>
       )}
       {/* {addActivityPopUp ? <AddActivityPopup /> : <Footer />} */}
     </>
