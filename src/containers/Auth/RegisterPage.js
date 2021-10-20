a379d7af0c031bd7ca356d26549f9ab4bfbccd77
Atabic <atabic14@gmail.com>
2021-10-20 15:38:54 +0500

    CUR-2281

--- src/containers/Auth/RegisterPage.js
+++ src/containers/Auth/RegisterPage.js
@@ -457,7 +457,7 @@ class RegisterPage extends React.Component {
                         className="input-box"
                         name="organizationName"
                         placeholder="Organization Name*"
-                        maxLength="250"
+                        maxLength="50"
                         value={organizationName}
                         onChange={this.onChangeField}
                       />
@@ -468,7 +468,7 @@ class RegisterPage extends React.Component {
                         className="input-box"
                         name="jobTitle"
                         placeholder="Job Title*"
-                        maxLength="250"
+                        maxLength="50"
                         value={jobTitle}
                         onChange={this.onChangeField}
                       />
