2f6a29ecb01ed0db9cb8f855caa2e491f12bea41
Atabic <atabic14@gmail.com>
2021-10-26 15:42:38 +0500

    log removed

--- src/containers/Playlists/index.js
+++ src/containers/Playlists/index.js
@@ -277,7 +277,6 @@ function PlaylistsPage(props) {
     }
   };
   const onBlur = (e) => {
-    console.log(e.target.value, "value");
     if (e.target.name === 'projectname') {
       titleRef.current.blur();
       setEditName(false);
