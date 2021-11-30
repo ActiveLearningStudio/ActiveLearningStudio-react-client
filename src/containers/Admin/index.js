3cc448aaca1228277e5316e305fff8cd38d4591a
Muhammad <muhammadqamar111@gmail.com>
2021-11-30 21:49:38 +0500

    sidebar

--- src/containers/Admin/index.js
+++ src/containers/Admin/index.js
@@ -5,26 +5,26 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useDispatch, useSelector } from 'react-redux';
 import { setActiveAdminForm } from 'store/actions/admin';
 
-import { removeActiveAdminForm, setActiveTab } from "store/actions/admin";
-import CreateActivityItem from "./formik/createActivityItem";
-import CreateActivityType from "./formik/createActivity";
-import CreateOrg from "./formik/createOrg";
-import AddRole from "./formik/addRole";
-import CreateUser from "./formik/createuser";
-import Pills from "./pills";
-import Heading from "./heading";
-import Breadcrump from "utils/BreadCrump/breadcrump";
-import * as actionTypes from "store/actionTypes";
-import CreateLms from "./formik/createLms";
-import CreateDefaultSso from "./formik/createDefaultSso";
-import CreateLtiTool from "./formik/createLtiTool";
-import "./style.scss";
-import { getRoles } from "store/actions/organization";
-import EditProject from "./formik/editProject";
-import { useHistory } from "react-router-dom";
+import { removeActiveAdminForm, setActiveTab } from 'store/actions/admin';
+import CreateActivityItem from './formik/createActivityItem';
+import CreateActivityType from './formik/createActivity';
+import CreateOrg from './formik/createOrg';
+import AddRole from './formik/addRole';
+import CreateUser from './formik/createuser';
+import Pills from './pills';
+import Heading from './heading';
+import Breadcrump from 'utils/BreadCrump/breadcrump';
+import * as actionTypes from 'store/actionTypes';
+import CreateLms from './formik/createLms';
+import CreateDefaultSso from './formik/createDefaultSso';
+import CreateLtiTool from './formik/createLtiTool';
+import './style.scss';
+import { getRoles } from 'store/actions/organization';
+import EditProject from './formik/editProject';
+import { useHistory } from 'react-router-dom';
 import editicon from 'assets/images/edit-icon.png';
 
-function AdminPanel() {
+function AdminPanel({ showSSO }) {
   const history = useHistory();
   const dispatch = useDispatch();
   const [allProjectTab, setAllProjectTab] = useState(null);
@@ -38,7 +38,7 @@ function AdminPanel() {
       dispatch(getRoles());
     }
   }, [activeOrganization]);
-  useEffect(() => { }, [activeTab]);
+  useEffect(() => {}, [activeTab]);
   useEffect(() => {
     const tab = localStorage.getItem('activeTab');
     if (tab) {
@@ -61,83 +61,101 @@ function AdminPanel() {
             <div className="inner-content">
               <Breadcrump />
               <Heading />
-              <Tabs
-                defaultActiveKey={activeTab}
-                activeKey={activeTab}
-                id="uncontrolled-tab-example"
-                onSelect={(key) => {
-                  dispatch(setActiveTab(key));
-                  localStorage.setItem('activeTab', key);
-                }}
-              >
-                <Tab eventKey="Organization" title="Organizations">
-                  <div className="parent-organization-detail">
-                    <div className="detailer">
-                      <h3>Main organization: {currentOrganization.name}</h3>
-                      <p>{currentOrganization.description}</p>
+              {!showSSO ? (
+                <Tabs
+                  defaultActiveKey={activeTab}
+                  activeKey={activeTab}
+                  id="uncontrolled-tab-example"
+                  onSelect={(key) => {
+                    dispatch(setActiveTab(key));
+                    localStorage.setItem('activeTab', key);
+                  }}
+                >
+                  <Tab eventKey="Organization" title="Organizations">
+                    <div className="parent-organization-detail">
+                      <div className="detailer">
+                        <h3>Main organization: {currentOrganization.name}</h3>
+                        <p>{currentOrganization.description}</p>
+                      </div>
+                      <button
+                        onClick={() => {
+                          dispatch(setActiveAdminForm('edit_org'));
+                          dispatch({
+                            type: 'SET_ACTIVE_EDIT',
+                            payload: activeOrganization,
+                          });
+                        }}
+                      >
+                        <img src={editicon} alt="" />
+                        Edit organization
+                      </button>
                     </div>
-                    <button
-                      onClick={() => {
-                        dispatch(setActiveAdminForm('edit_org'));
-                        dispatch({
-                          type: 'SET_ACTIVE_EDIT',
-                          payload: activeOrganization,
-                        });
-                      }}
-                    >
-                      <img src={editicon} alt="" />
-                      Edit organization
-                    </button>
-                  </div>
-                  <div className="module-content">
-                    <Pills modules={['All Organizations']} type="Organization" subType="All Organizations" />
-                  </div>
-                </Tab>
-                {permission?.Project?.includes('project:view') && (
-                  <Tab eventKey="Project" title="Projects">
                     <div className="module-content">
-                      <Pills modules={['All Projects', 'Library requests', 'Exported Projects']} allProjectTab={allProjectTab} setAllProjectTab={setAllProjectTab} type="Project" />
+                      <Pills modules={['All Organizations']} type="Organization" subType="All Organizations" />
                     </div>
                   </Tab>
-                )}
-                <Tab eventKey="Activities" title="Activities">
-                  <div className="module-content">
-                    <Pills modules={['Activity Types', 'Activity Items']} type="Activities" />
-                  </div>
-                </Tab>
-                {permission?.Organization?.includes('organization:view-user') && (
-                  <Tab eventKey="Users" title="Users">
+                  {permission?.Project?.includes('project:view') && (
+                    <Tab eventKey="Project" title="Projects">
+                      <div className="module-content">
+                        <Pills
+                          modules={['All Projects', 'Library requests', 'Exported Projects']}
+                          allProjectTab={allProjectTab}
+                          setAllProjectTab={setAllProjectTab}
+                          type="Project"
+                        />
+                      </div>
+                    </Tab>
+                  )}
+                  <Tab eventKey="Activities" title="Activities">
                     <div className="module-content">
-                      <Pills
-                        modules={[
-                          'All Users',
-                          permission?.Organization?.includes('organization:add-role') || permission?.Organization?.includes('organization:edit-role') ? 'Manage Roles' : null,
-                        ]}
-                        type="Users"
-                        subType="All Users"
-                      />
+                      <Pills modules={['Activity Types', 'Activity Items']} type="Activities" />
                     </div>
                   </Tab>
-                )}
-                <Tab eventKey="LMS" title="Integrations">
-                  <div className="module-content">
-                    <Pills modules={["All Settings", "LTI Tools"]} type="LMS" />
-                  </div>
-                </Tab>
-                {/* <Tab eventKey="Settings" title="Settings">
+                  {permission?.Organization?.includes('organization:view-user') && (
+                    <Tab eventKey="Users" title="Users">
+                      <div className="module-content">
+                        <Pills
+                          modules={[
+                            'All Users',
+                            permission?.Organization?.includes('organization:add-role') || permission?.Organization?.includes('organization:edit-role') ? 'Manage Roles' : null,
+                          ]}
+                          type="Users"
+                          subType="All Users"
+                        />
+                      </div>
+                    </Tab>
+                  )}
+                  <Tab eventKey="LMS" title="Integrations">
+                    <div className="module-content">
+                      <Pills modules={['All Settings', 'LTI Tools']} type="LMS" />
+                    </div>
+                  </Tab>
+                  {/* <Tab eventKey="Settings" title="Settings">
                   <div className="module-content">
                     <h2>Settings</h2>
                     <Pills modules={["All settings"]} type="Settings" />
                   </div>
                 </Tab> */}
-                {permission?.Organization?.includes('organization:view-default-sso') && (
-                  <Tab eventKey="DefaultSso" title="Default SSO Integrations">
-                    <div className="module-content">
-                      <Pills modules={['All Default SSO Settings']} type="DefaultSso" />
-                    </div>
-                  </Tab>
-                )}
-              </Tabs>
+                </Tabs>
+              ) : (
+                <Tabs
+                  defaultActiveKey={'DefaultSso'}
+                  activeKey={'DefaultSso'}
+                  id="uncontrolled-tab-example"
+                  onSelect={(key) => {
+                    dispatch(setActiveTab(key));
+                    localStorage.setItem('activeTab', key);
+                  }}
+                >
+                  {permission?.Organization?.includes('organization:view-default-sso') && (
+                    <Tab eventKey="DefaultSso" title="Default SSO Integrations">
+                      <div className="module-content">
+                        <Pills modules={['All Default SSO Settings']} type="DefaultSso" />
+                      </div>
+                    </Tab>
+                  )}
+                </Tabs>
+              )}
             </div>
           </div>
           {(activeForm === 'add_activity_type' || activeForm === 'edit_activity_type') && (
@@ -287,25 +305,18 @@ function AdminPanel() {
             </div>
           )}
 
-          {(activeForm === "add_lti_tool" ||
-            activeForm === "edit_lti_tool") && (
-              <div className="form-new-popup-admin">
-                <FontAwesomeIcon
-                  icon="times"
-                  className="cross-all-pop"
-                  onClick={() => {
-                    dispatch(removeActiveAdminForm());
-                  }}
-                />
-                <div className="inner-form-content">
-                  {activeForm === "add_lti_tool" ? (
-                    <CreateLtiTool />
-                  ) : (
-                    <CreateLtiTool editMode />
-                  )}
-                </div>
-              </div>
-            )}
+          {(activeForm === 'add_lti_tool' || activeForm === 'edit_lti_tool') && (
+            <div className="form-new-popup-admin">
+              <FontAwesomeIcon
+                icon="times"
+                className="cross-all-pop"
+                onClick={() => {
+                  dispatch(removeActiveAdminForm());
+                }}
+              />
+              <div className="inner-form-content">{activeForm === 'add_lti_tool' ? <CreateLtiTool /> : <CreateLtiTool editMode />}</div>
+            </div>
+          )}
         </>
       ) : (
         <div className="content-wrapper" style={{ padding: '20px' }}>
