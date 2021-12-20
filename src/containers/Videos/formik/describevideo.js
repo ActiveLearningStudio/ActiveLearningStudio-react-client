f5c8cf48ab79642a5445cc409f21c03841698866
Muhammad <muhammadqamar111@gmail.com>
2021-12-20 10:54:33 +0500

    added new videos

--- src/containers/Videos/formik/describevideo.js
+++ src/containers/Videos/formik/describevideo.js
@@ -1,29 +1,33 @@
 /*eslint-disable*/
-import React, { useEffect, useState } from "react";
-import HeadingTwo from "utils/HeadingTwo/headingtwo";
-import TabsHeading from "utils/Tabs/tabs";
-import { Tabs, Tab } from "react-bootstrap";
-import { Formik } from "formik";
-import Buttons from "utils/Buttons/buttons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faClock } from "@fortawesome/free-solid-svg-icons";
-import BrightcoveModel from "../model/brightmodel";
-import UploadImageV2 from "utils/uploadimagev2/uploadimagev2";
-import UploadImage from "utils/UploadImage/uploadimage";
-import HeadingText from "utils/HeadingText/headingtext";
-import DefaultUpload from "assets/images/defaultUpload.png";
+import React, { useEffect, useState } from 'react';
+import HeadingTwo from 'utils/HeadingTwo/headingtwo';
+import TabsHeading from 'utils/Tabs/tabs';
+import { Tabs, Tab } from 'react-bootstrap';
+import { Formik } from 'formik';
+import Buttons from 'utils/Buttons/buttons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-solid-svg-icons';
+import BrightcoveModel from '../model/brightmodel';
+import UploadImageV2 from 'utils/uploadimagev2/uploadimagev2';
+import UploadImage from 'utils/UploadImage/uploadimage';
+import HeadingText from 'utils/HeadingText/headingtext';
+import DefaultUpload from 'assets/images/defaultUpload.png';
+import PreviewLayoutModel from 'containers/MyProject/model/previewlayout';
 const DescribeVideo = ({ setUploadImageStatus }) => {
   const [modalShow, setModalShow] = useState(false);
   return (
     <>
+      <PreviewLayoutModel
+        show={modalShow}
+        onHide={() => {
+          setModalShow(false);
+        }}
+        type="videoModal"
+      />
       <div className="add-describevideo-form">
         <div className="add-describevideo-tabs">
           <TabsHeading text="1. Add a video" tabActive={true} />
-          <TabsHeading
-            text="2. Describe video"
-            className="ml-10"
-            tabActive={true}
-          />
+          <TabsHeading text="2. Describe video" className="ml-10" tabActive={true} />
           <TabsHeading text="3. Add interactions" className="ml-10" />
           <TabsHeading text="4. Optional settings" className="ml-10" />
         </div>
@@ -33,11 +37,7 @@ const DescribeVideo = ({ setUploadImageStatus }) => {
           </div>
           <div className="add-describevideo-tour">
             <span>
-              <FontAwesomeIcon
-                icon={faClock}
-                color="#084892"
-                className="ml-9"
-              />
+              <FontAwesomeIcon icon={faClock} color="#084892" className="ml-9" />
               Tour
             </span>
           </div>
@@ -46,19 +46,14 @@ const DescribeVideo = ({ setUploadImageStatus }) => {
           <div className="add-describevideo-layout-formik">
             <Formik
               initialValues={{
-                videoUrl: "",
+                title: '',
+                description: '',
+                subject_id: '',
+                education_level_id: '',
               }}
               onSubmit={(values) => {}}
             >
-              {({
-                values,
-                errors,
-                touched,
-                handleChange,
-                handleBlur,
-                handleSubmit,
-                isSubmitting,
-              }) => (
+              {({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
                 <form
                   onSubmit={(e) => {
                     e.preventDefault();
@@ -68,64 +63,22 @@ const DescribeVideo = ({ setUploadImageStatus }) => {
                   <div className="dec-title-formik-textField">
                     <span>Title</span>
                     <p>Used for searching, reports and copyright information</p>
-                    <input
-                      type="text"
-                      name="title"
-                      placeholder="Give your layout a name..."
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      value={values.title}
-                    />
+                    <input type="text" name="title" placeholder="Give your layout a name..." onChange={handleChange} onBlur={handleBlur} value={values.title} />
                   </div>
                   <div className="dec-title-formik-textField">
                     <span>Description</span>
-                    <textarea
-                      rows="4"
-                      cols="4"
-                      name="description"
-                      placeholder="What is this video about"
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      value={values.title}
-                    />
+                    <textarea rows="4" cols="4" name="description" placeholder="What is this video about" onChange={handleChange} onBlur={handleBlur} value={values.description} />
                   </div>
                   <div className="layout-formik-select">
                     <div className="formik-select mr-32">
-                      <HeadingText
-                        text="Subject"
-                        className="formik-select-title"
-                      />
-                      <select
-                        name="subject_id"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                      >
+                      <HeadingText text="Subject" className="formik-select-title" />
+                      <select name="subject_id" onChange={handleChange} onBlur={handleBlur} value={values.subject_id}>
                         <option hidden>Select</option>
                       </select>
                     </div>
                     <div className="formik-select mr-32 ">
-                      <HeadingText
-                        text="Education level"
-                        className="formik-select-title"
-                      />
-                      <select
-                        name="education_level_id"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                      >
-                        <option hidden>Select</option>
-                      </select>
-                    </div>
-                    <div className="formik-select ">
-                      <HeadingText
-                        text="Author tags"
-                        className="formik-select-title"
-                      />
-                      <select
-                        name="subject_id"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                      >
+                      <HeadingText text="Education level" className="formik-select-title" />
+                      <select name="education_level_id" onChange={handleChange} onBlur={handleBlur} value={values.education_level_id}>
                         <option hidden>Select</option>
                       </select>
                     </div>
@@ -135,23 +88,17 @@ const DescribeVideo = ({ setUploadImageStatus }) => {
             </Formik>
           </div>
           <div className="describe-video-upload-section">
-            <UploadImage
-              title="Upload poster (Optional)"
-              defuaultImage={DefaultUpload}
-              className="uploadImage-describe-video"
-              setUploadImageStatus={setUploadImageStatus}
-            />
+            <UploadImage title="Upload poster (Optional)" defuaultImage={DefaultUpload} className="uploadImage-describe-video" setUploadImageStatus={setUploadImageStatus} />
           </div>
         </div>
 
-        <div className="describe-video">
-          <Buttons
-            primary={true}
-            text="Add Interactions"
-            width="162px"
-            height="32px"
-            hover={true}
-          />
+        <div
+          className="describe-video"
+          onClick={() => {
+            setModalShow(true);
+          }}
+        >
+          <Buttons primary={true} text="Add Interactions" width="162px" height="32px" hover={true} />
         </div>
       </div>
     </>
