c68df81159c31d52b6393c73c6346f63432e9ec4
muhammadqamar <muhammadqamar111@gmail.com>
2022-02-26 00:11:02 +0500

    canvas design update

--- src/containers/LMS/Canvas/DeepLinking/SearchResults/index.js
+++ src/containers/LMS/Canvas/DeepLinking/SearchResults/index.js
@@ -1,10 +1,9 @@
+/* eslint-disable */
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import {
-  Alert,
-} from 'react-bootstrap';
+import { Alert, Tabs, Tab } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import Swal from 'sweetalert2';
 import {
@@ -17,19 +16,10 @@ import {
 } from 'store/actions/canvas';
 // import PreviewActivity from 'containers/LMS/Canvas/DeepLinking/PreviewActivity';
 import Project from 'containers/LMS/Canvas/DeepLinking/Project';
-// import './style.scss';
+import './style.scss';
 
 const SearchResults = (props) => {
-  const {
-    match,
-    searchParams,
-    hasMoreResults,
-    projects,
-    backToSearch,
-    previousPage,
-    nextPage,
-    search,
-  } = props;
+  const { match, searchParams, hasMoreResults, projects, backToSearch, previousPage, nextPage, search } = props;
 
   // Init
   useEffect(() => {
@@ -39,13 +29,13 @@ const SearchResults = (props) => {
 
   return (
     <div className="results">
-      <div className="row">
+      <div className="row my-4">
         <div className="col">
-          <h2>Search Results</h2>
+          <h4 className="search-heading">Search Results</h4>
         </div>
         <div className="col text-right">
-          <button type="button" className="btn back-action" onClick={backToSearch}>
-            <FontAwesomeIcon icon="chevron-left" className="action-icon" />
+          <button type="button" className="btn back-action" style={{ color: 'rgb(8, 72, 146) ' }} onClick={backToSearch}>
+            <FontAwesomeIcon icon="chevron-left" className="action-icon mr-2" />
             Back to Search
           </button>
         </div>
@@ -53,27 +43,32 @@ const SearchResults = (props) => {
       {projects !== null && projects.length === 0 && (
         <div className="row">
           <div className="col">
-            <Alert variant="warning">
-              No results found.
-            </Alert>
+            <Alert variant="warning">No results found.</Alert>
           </div>
         </div>
       )}
-      {projects !== null && projects.length > 0 && projects.map((project) => (
-        <Project project={project} key={project.id} />
-      ))}
-      <div className="row">
-        <div className="col text-left">
-          {(!!searchParams.from && searchParams.from !== 0) && (
-            <button type="button" className="btn btn-primary pagination-buttons" onClick={previousPage}>Previous</button>
-          )}
-        </div>
-        <div className="col text-right">
-          {hasMoreResults && (
-            <button type="button" className="btn btn-primary pagination-buttons" onClick={nextPage}>Next</button>
-          )}
-        </div>
-      </div>
+      <Tabs defaultActiveKey="project" id="uncontrolled-tab-example">
+        <Tab eventKey="project" title="Project">
+          {projects !== null && projects.length > 0 && projects.map((project) => <Project project={project} key={project.id} />)}
+          <div className="row">
+            <div className="col text-left">
+              {!!searchParams.from && searchParams.from !== 0 && (
+                <button type="button" className="btn btn-primary pagination-buttons" onClick={previousPage}>
+                  Previous
+                </button>
+              )}
+            </div>
+            <div className="col text-right">
+              {hasMoreResults && (
+                <button type="button" className="btn btn-primary pagination-buttons" onClick={nextPage}>
+                  Next
+                </button>
+              )}
+            </div>
+          </div>
+        </Tab>
+        <Tab eventKey="Playlists" title="Playlists"></Tab>
+      </Tabs>
     </div>
   );
 };
