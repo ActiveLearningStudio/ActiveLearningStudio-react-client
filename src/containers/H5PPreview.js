61020f8ad3f322241af759f4f20d5517f7b2d603
CptTony <antonioetayo@gmail.com>
2022-03-23 09:44:35 -0400

    Merge pull request #1096 from ActiveLearningStudio/hotfix/shared_link_activities_fix
    Shared project link fixes

--- src/containers/H5PPreview.js
+++ src/containers/H5PPreview.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import PropTypes from 'prop-types';
 import { connect, useDispatch, useSelector } from 'react-redux';
 import { withRouter } from 'react-router-dom';
@@ -17,57 +17,100 @@ const H5PPreview = (props) => {
   const [resourceId, setResourceId] = useState(null);
 
   const { activityId, loadH5pResourceProp, showLtiPreview, showActivityPreview, showvideoH5p } = props;
+  const initialActivityState = {
+    intervalId: null,
+    assets: [],
+    assetsLoaded: [],
+    h5pObject: null,
+  };
 
-  const dispatch = useDispatch();
+  const reducer = (activityState, action) => {
+    switch (action.type) {
+      case 'SET_INTERVAL':
+        return {
+          ...activityState,
+          intervalId: action.intervalId,
+        };
+
+      case 'ASSETS_LOADED':
+        return {
+          ...activityState,
+          assetsLoaded: [...activityState.assetsLoaded, action.asset],
+        };
+
+      case 'CHECK_ASSETS':
+        console.log('checking assets');
+        if (typeof window.H5P === 'undefined' || !window.H5P.externalDispatcher) {
+          console.log('H5P is not ready yet...');
+          return activityState;
+        }
+
+        if (activityState.assets.length !== activityState.assetsLoaded.length) {
+          console.log(`Assets not ready. ${activityState.assetsLoaded.length} of ${activityState.assets.length} loaded`);
+          return activityState;
+        }
+
+        clearInterval(activityState.intervalId);
+        return {
+          ...activityState,
+          h5pObject: window.H5P,
+          intervalId: null,
+        };
+
+      case 'SET_ASSETS':
+        return {
+          ...activityState,
+          assets: action.assets,
+        };
+      case 'RESET_STATE':
+        return initialActivityState;
+      default:
+        return activityState;
+    }
+  };
+
+  // We do use it with the reducer
+  /* eslint-disable-next-line no-unused-vars */
+  const [activityState, dispatch] = useReducer(reducer, initialActivityState);
+
+  const loadAssets = (styles, scripts) => {
+    styles.forEach((style) => {
+      const link = document.createElement('link');
+      link.href = style;
+      link.type = 'text/css';
+      link.rel = 'stylesheet';
+      document.head.appendChild(link);
+    });
+    scripts.forEach((script) => {
+      const element = document.createElement('script');
+      element.onload = () => {
+        dispatch({ type: 'ASSETS_LOADED', asset: element.src });
+        console.log(`Assets loaded: ${element.src}`);
+      };
+      element.src = script;
+      element.async = false;
+      document.body.appendChild(element);
+    });
+  };
 
   const resourceLoaded = async (data) => {
+    window.H5P = window.H5P || {};
+    window.H5P.preventInit = true;
     window.H5PIntegration = data.h5p.settings;
     const h5pWrapper = document.getElementById('curriki-h5p-wrapper');
     h5pWrapper.innerHTML = data.h5p.embed_code.trim();
-    const newCss = data.h5p.settings.core.styles.concat(data.h5p.settings.loadedCss);
-
-    const h5pContentKeys = Object.keys(window.H5PIntegration.contents);
-    const h5pContent = h5pContentKeys.length > 0 ? window.H5PIntegration.contents[h5pContentKeys[0]] : undefined;
-    const divEmbedLibraries = ['H5P.BrightcoveInteractiveVideo 1.0', 'H5P.IFrameEmbed 1.0'];
-    const isDivEmbedLibrary = divEmbedLibraries.includes(h5pContent?.library);
-
-    if (isDivEmbedLibrary) {
-      window.H5P = window.H5P || {};
-      window.H5P.preventInit = true;
-    }
-
-    await Promise.all(
-      newCss.map((value) => {
-        const link = document.createElement('link');
-        link.href = value;
-        link.type = 'text/css';
-        link.rel = 'stylesheet';
-        link.title = 'brightcove';
-        document.head.appendChild(link);
-        return true;
-      })
-    );
-
+    const newStyles = data.h5p.settings.core.styles.concat(data.h5p.settings.loadedCss);
     const newScripts = data.h5p.settings.core.scripts.concat(data.h5p.settings.loadedJs);
 
-    newScripts.forEach((value) => {
-      const script = document.createElement('script');
-      script.src = value;
-      script.async = false;
-      document.body.appendChild(script);
-    });
+    // Load H5P assets
+    dispatch({ type: 'SET_ASSETS', assets: newScripts });
+    loadAssets(newStyles, newScripts);
 
-    if (isDivEmbedLibrary) {
-      const h5pLibLoadTime = setInterval(function (e) {
-        const libObjectName = h5pContent?.library?.split(' ')[0]?.split('H5P.')?.filter(Boolean);
-        if (libObjectName in window.H5P) {
-          clearInterval(h5pLibLoadTime);
-          window.H5P.init(document.body); // execute H5P
-          window.H5P.preventInit = undefined;
-        }
-      }, 300);
-    }
-
+    // Loops until H5P object and dispatcher are ready
+    const intervalId = setInterval(() => {
+      dispatch({ type: 'CHECK_ASSETS' });
+    }, 500);
+    dispatch({ type: 'SET_INTERVAL', intervalId });
     setLoading(false);
   };
 
@@ -87,7 +130,7 @@ const H5PPreview = (props) => {
           if (showLtiPreview) {
             const response = await loadH5pResourceSettingsOpen(activityId);
             if (response.h5p_activity) {
-              window.brightcoveAccountId = response.h5p_activity?.brightcoveData.accountId;
+              window.brightcoveAccountId = response.h5p_activity?.brightcoveData?.accountId;
               await resourceLoaded(response.h5p_activity);
             }
           } else if (showActivityPreview) {
@@ -98,7 +141,7 @@ const H5PPreview = (props) => {
           } else if (showvideoH5p) {
             const response = await videoServices.renderh5pvideo(activeOrganization.id, activityId);
             if (response.activity?.brightcoveData) {
-              window.brightcoveAccountId = response.activity?.brightcoveData.accountId;
+              window.brightcoveAccountId = response.activity?.brightcoveData?.accountId;
             }
             if (response.activity) {
               await resourceLoaded(response.activity);
@@ -112,34 +155,32 @@ const H5PPreview = (props) => {
         } catch (e) {
           setLoading(false);
         }
-
-        const checkXapi = setInterval(() => {
-          try {
-            const x = document.getElementsByClassName('h5p-iframe')[0].contentWindow;
-            if (x.H5P) {
-              if (x.H5P.externalDispatcher && xAPIHelper.isxAPINeeded(props.match.path)) {
-                // eslint-disable-next-line no-use-before-define
-                stopXapi();
-
-                x.H5P.externalDispatcher.on('xAPI', (event) => {
-                  if (counter > 0) {
-                    dispatch(loadH5pResourceXapi(JSON.stringify(xAPIHelper.extendStatement(event.data.statement, { ...props }))));
-                  }
-                  counter += 1;
-                });
-              }
-            }
-          } catch (e) {}
-        });
-
-        const stopXapi = () => clearInterval(checkXapi);
       };
 
+      dispatch({ type: 'RESET_STATE' });
       loadResource();
 
       setResourceId(activityId);
     }
   }, [resourceId, activityId, showLtiPreview, showActivityPreview, loadH5pResourceProp]);
+
+  // Init h5p Object once it is ready
+  useEffect(() => {
+    if (!activityState.h5pObject) {
+      console.log('H5P object not ready');
+      return;
+    }
+    if (activityState.h5pObject.externalDispatcher && xAPIHelper.isxAPINeeded(props.match.path)) {
+      // eslint-disable-next-line no-use-before-define
+      activityState.h5pObject.externalDispatcher.on('xAPI', (event) => {
+        if (counter > 0) {
+          dispatch(loadH5pResourceXapi(JSON.stringify(xAPIHelper.extendStatement(event.data.statement, { ...props }))));
+        }
+        counter += 1;
+      });
+    }
+    activityState.h5pObject.init();
+  }, [activityState.h5pObject]);
 
   return (
     <>
