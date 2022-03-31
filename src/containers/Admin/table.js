59ff61956d1713ad298b02e30b3e1ca3d2550e00
muhammadqamar <muhammadqamar111@gmail.com>
2022-03-31 18:54:22 +0500

    notification link

--- src/containers/Admin/table.js
+++ src/containers/Admin/table.js
@@ -719,6 +719,7 @@ function Table(props) {
               (localStateData ? (
                 localStateData?.length > 0 ? (
                   localStateData?.map((row, counter) => {
+                    console.log(row);
                     return (
                       <tr key={counter} className="org-rows">
                         <td>{row.project}</td>
