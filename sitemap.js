b64cd32025f2c73f2affd259f5e9c55dd4a2dc12
muhammadqamar <muhammadqamar111@gmail.com>
2022-05-13 21:36:05 +0500

    fix domain

--- sitemap.js
+++ sitemap.js
@@ -2,8 +2,7 @@
 
 import Generator from 'react-router-sitemap-generator';
 import Router from './router'; //import your react router component
-
-const generator = new Generator('https://react-router-sitemap-generator.com', Router(), {
+const generator = new Generator(process.env.DOMAIN_URL, Router(), {
   lastmod: new Date().toISOString().slice(0, 10),
   changefreq: 'monthly',
   priority: 0.8,
