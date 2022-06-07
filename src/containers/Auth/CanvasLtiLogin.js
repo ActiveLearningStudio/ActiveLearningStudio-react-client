44798e005fa25ba1456620bf4eefb9cfd6051b8a
DESKTOP-V9B7HMN\TK-LPT-0358 <mohsin.iftikhar@tkxel.com>
2022-06-07 19:38:07 +0500

    Redirection in case of safarimontage sso to activities page

--- src/containers/Auth/CanvasLtiLogin.js
+++ src/containers/Auth/CanvasLtiLogin.js
@@ -9,6 +9,17 @@ import { CanvasSSOLoginAction } from 'store/actions/auth';
 import Logo from './Logo';
 import './style.scss';
 
+function _base64ToArray(base64String){
+  let resulted_arr = [];
+  let decoded_val = atob(base64String).split('&');
+  if(decoded_val.length > 0) {
+    for (let i = 0; i < decoded_val.length; i++) {
+      let spliting_val = decoded_val[i].split('=');
+      resulted_arr[spliting_val[0]]=spliting_val[1];
+    }
+  }
+  return resulted_arr;
+}
 function CanvasLtiSSO(props) {
 	const { history } = props;
 	const dispatch = useDispatch();
@@ -17,8 +28,14 @@ function CanvasLtiSSO(props) {
 			const query = QueryString.parse(window.location.search);
 			if (query.sso_info) {
 				const result = dispatch(CanvasSSOLoginAction({ sso_info: query.sso_info }));
-				result.then(() => {
-					history.push('/');
+				result.then((data) => {
+          const domain = data.user.user_organization;
+          if(typeof _base64ToArray(query.sso_info)['redirect'] !== "undefined"){
+            history.push(`/org/${domain?.domain}/activities`);
+          }
+          else {
+				  	history.push('/');
+          }
 				}).catch((err) => {
                     Swal.fire({
 						icon:'error',
