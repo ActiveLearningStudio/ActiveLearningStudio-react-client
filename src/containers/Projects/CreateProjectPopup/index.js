761ed9e24fc6688a8c8ed3198fd928c0c96881f9
muhammadqamar <muhammadqamar111@gmail.com>
2022-07-13 16:29:48 +0500

    redirect to playlist

--- src/containers/Projects/CreateProjectPopup/index.js
+++ src/containers/Projects/CreateProjectPopup/index.js
@@ -25,37 +25,33 @@ let imageValidation = '';
 const projectShare = true;
 
 const onSubmit = async (values, dispatch, props) => {
-  const {
-    history,
-    project,
-    fromTeam,
-    selectedTeam,
-    handleCloseProjectModal,
-  } = props;
+  const { history, project, fromTeam, selectedTeam, handleCloseProjectModal, currentOrganization } = props;
   const { name, description, vType } = values;
   const result = await dispatch(
     project?.thumbUrl
       ? createProjectAction({
-        name,
-        description,
-        thumb_url: project?.thumbUrl,
-        is_public: projectShare,
-        organization_visibility_type_id: 1,
-        team_id: fromTeam && selectedTeam ? selectedTeam?.id : null
-      })
+          name,
+          description,
+          thumb_url: project?.thumbUrl,
+          is_public: projectShare,
+          organization_visibility_type_id: 1,
+          team_id: fromTeam && selectedTeam ? selectedTeam?.id : null,
+        })
       : createProjectAction({
-        name,
-        description,
-        is_public: projectShare,
-        organization_visibility_type_id: 1,
-        team_id: fromTeam && selectedTeam ? selectedTeam?.id : null,
-        // eslint-disable-next-line max-len
-        thumb_url: 'https://images.pexels.com/photos/593158/pexels-photo-593158.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=1&amp;fit=crop&amp;h=200&amp;w=280',
-      })
+          name,
+          description,
+          is_public: projectShare,
+          organization_visibility_type_id: 1,
+          team_id: fromTeam && selectedTeam ? selectedTeam?.id : null,
+          // eslint-disable-next-line max-len
+          thumb_url: 'https://images.pexels.com/photos/593158/pexels-photo-593158.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=1&amp;fit=crop&amp;h=200&amp;w=280',
+        })
   );
   handleCloseProjectModal(false);
   if (result && !fromTeam) {
+    console.log(result);
     history.push('/projects');
+    history.push(`/org/${currentOrganization?.currentOrganization?.domain}/project/${result.id}`);
   }
 };
 export const uploadThumb = async (e, permission, teamPermission, id, dispatch) => {
@@ -87,7 +83,7 @@ let CreateProjectPopup = (props) => {
   const stateHeader = useSelector((state) => state.organization);
   const projectState = useSelector((state) => state.project);
   const { teamPermission } = useSelector((state) => state.team);
-  const { permission } = stateHeader;
+  const { permission, currentOrganization } = stateHeader;
   const [modalShow, setModalShow] = useState(false);
   const openFile = useRef();
   const [visibilityTypeArray, setVisibilityTypeArray] = useState([]);
@@ -100,7 +96,6 @@ let CreateProjectPopup = (props) => {
     },
     [handleCloseProjectModal]
   );
-
 
   useEffect(() => {
     document.addEventListener('keydown', escFunction, false);
@@ -115,135 +110,133 @@ let CreateProjectPopup = (props) => {
     })();
   }, [getProjectVisibilityTypes]);
 
-  return (
-    (permission?.Project?.includes('project:create')) ? (
-      <div className="create-program-wrapper">
-        <PexelsAPI
-          show={modalShow}
-          project={project}
-          onHide={() => {
-            setModalShow(false);
-          }}
-          searchName="abstract"
-        />
+  return permission?.Project?.includes('project:create') ? (
+    <div className="create-program-wrapper">
+      <PexelsAPI
+        show={modalShow}
+        project={project}
+        onHide={() => {
+          setModalShow(false);
+        }}
+        searchName="abstract"
+      />
 
-        <form className="create-playlist-form" onSubmit={handleSubmit} autoComplete="off">
-          <div className="project-name">
-            <Field
-              name="name"
-              component={InputField}
-              type="text"
-              validate={[required, maxLength80]}
-              autoComplete="new-password"
-              className="reduxlabel"
-              label="Project name"
-              placeholder="e.g Course Name"
-            />
-          </div>
+      <form className="create-playlist-form" onSubmit={handleSubmit} autoComplete="off">
+        <div className="project-name">
+          <Field
+            name="name"
+            component={InputField}
+            type="text"
+            validate={[required, maxLength80]}
+            autoComplete="new-password"
+            className="reduxlabel"
+            label="Project name"
+            placeholder="e.g Course Name"
+          />
+        </div>
 
-          <div className="project-description">
-            <Field name="description" component={TextareaField} validate={[required, maxLength1000]} autoComplete="new-password" label="Project Description" />
-          </div>
-          <div className="upload-thumbnail check">
-            <div className="upload_placeholder">
-              <label style={{ display: 'none' }}>
-                <input
-                  ref={openFile}
-                  type="file"
-                  accept="image/x-png,image/jpeg"
-                  onChange={(e) => {
-                    if (e.target.files.length === 0) {
-                      return true;
-                    }
-                    if (
-                      !(
-                        e.target.files[0].type.includes('png') ||
-                        e.target.files[0].type.includes('jpg') ||
-                        e.target.files[0].type.includes('gif') ||
-                        e.target.files[0].type.includes('jpeg')
-                      )
-                    ) {
-                      Swal.fire({
-                        icon: 'error',
-                        title: 'Error',
-                        text: 'Invalid file selected.',
-                      });
-                    } else if (e.target.files[0].size > 100000000) {
-                      Swal.fire({
-                        icon: 'error',
-                        title: 'Error',
-                        text: 'Selected file size should be less then 100MB.',
-                      });
-                    } else {
-                      uploadThumb(e, permission, teamPermission, projectState?.selectedProject?.id, dispatch);
-                    }
-                  }}
-                />
-                <span>Upload</span>
-              </label>
+        <div className="project-description">
+          <Field name="description" component={TextareaField} validate={[required, maxLength1000]} autoComplete="new-password" label="Project Description" />
+        </div>
+        <div className="upload-thumbnail check">
+          <div className="upload_placeholder">
+            <label style={{ display: 'none' }}>
+              <input
+                ref={openFile}
+                type="file"
+                accept="image/x-png,image/jpeg"
+                onChange={(e) => {
+                  if (e.target.files.length === 0) {
+                    return true;
+                  }
+                  if (
+                    !(
+                      e.target.files[0].type.includes('png') ||
+                      e.target.files[0].type.includes('jpg') ||
+                      e.target.files[0].type.includes('gif') ||
+                      e.target.files[0].type.includes('jpeg')
+                    )
+                  ) {
+                    Swal.fire({
+                      icon: 'error',
+                      title: 'Error',
+                      text: 'Invalid file selected.',
+                    });
+                  } else if (e.target.files[0].size > 100000000) {
+                    Swal.fire({
+                      icon: 'error',
+                      title: 'Error',
+                      text: 'Selected file size should be less then 100MB.',
+                    });
+                  } else {
+                    uploadThumb(e, permission, teamPermission, projectState?.selectedProject?.id, dispatch);
+                  }
+                }}
+              />
+              <span>Upload</span>
+            </label>
 
-              <span className="validation-error">{imageValidation}</span>
+            <span className="validation-error">{imageValidation}</span>
 
-              <div>
-                {project?.thumbUrl ? (
-                  <div className="thumb-display">
-                    <label>
-                      <h2>Upload Image</h2>
-                    </label>
-                    <div
-                      className="imgbox"
-                      style={{
-                        backgroundImage: project.thumbUrl.includes('pexels.com') ? `url(${project.thumbUrl})` : `url(${global.config.resourceUrl}${project.thumbUrl})`,
-                      }}
-                    />
-                  </div>
-                ) : (
-                  <div className="new-box">
-                    <label>
-                      <h2>Upload Image</h2>
-                    </label>
-                    <div className="imgbox">
-                      {/* eslint-disable-next-line max-len */}
-                      <img src="https://images.pexels.com/photos/593158/pexels-photo-593158.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=1&amp;fit=crop&amp;h=200&amp;w=280" alt="" />
-                    </div>
-                  </div>
-                )}
-              </div>
-
-              <div className="button-flex ">
-                <div className="pexel" onClick={() => setModalShow(true)}>
-                  <img src={pexel} alt="pexel" />
-                  <p>Pexels</p>
+            <div>
+              {project?.thumbUrl ? (
+                <div className="thumb-display">
+                  <label>
+                    <h2>Upload Image</h2>
+                  </label>
+                  <div
+                    className="imgbox"
+                    style={{
+                      backgroundImage: project.thumbUrl.includes('pexels.com') ? `url(${project.thumbUrl})` : `url(${global.config.resourceUrl}${project.thumbUrl})`,
+                    }}
+                  />
                 </div>
-                <div
-                  className="gallery"
-                  onClick={() => {
-                    openFile.current.click();
-                  }}
-                >
-                  <img src={computer} alt="" />
-                  <p>My device</p>
+              ) : (
+                <div className="new-box">
+                  <label>
+                    <h2>Upload Image</h2>
+                  </label>
+                  <div className="imgbox">
+                    {/* eslint-disable-next-line max-len */}
+                    <img src="https://images.pexels.com/photos/593158/pexels-photo-593158.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=1&amp;fit=crop&amp;h=200&amp;w=280" alt="" />
+                  </div>
                 </div>
-              </div>
+              )}
             </div>
 
-            <p className="disclaimer">
-              Project Image dimension should be <strong>280px width and 200px height. </strong>
-              Maximum File size allowed is <strong>100MB.</strong>
-            </p>
+            <div className="button-flex ">
+              <div className="pexel" onClick={() => setModalShow(true)}>
+                <img src={pexel} alt="pexel" />
+                <p>Pexels</p>
+              </div>
+              <div
+                className="gallery"
+                onClick={() => {
+                  openFile.current.click();
+                }}
+              >
+                <img src={computer} alt="" />
+                <p>My device</p>
+              </div>
+            </div>
           </div>
-          <div className="create-project-template-wrapper">
-            <button type="submit" className="create-project-submit-btn" disabled={isLoading}>
-              {isLoading ? <img src={loader} alt="" /> : 'Create Project'}
-            </button>
-          </div>
-        </form>
-      </div>
-    ) : (
-      <Alert style={{ marginTop: '25px' }} variant="danger">
-        You are not authorized to access this.
-      </Alert>
-    )
+
+          <p className="disclaimer">
+            Project Image dimension should be <strong>280px width and 200px height. </strong>
+            Maximum File size allowed is <strong>100MB.</strong>
+          </p>
+        </div>
+        <div className="create-project-template-wrapper">
+          <button type="submit" className="create-project-submit-btn" disabled={isLoading}>
+            {isLoading ? <img src={loader} alt="" /> : 'Create Project'}
+          </button>
+        </div>
+      </form>
+    </div>
+  ) : (
+    <Alert style={{ marginTop: '25px' }} variant="danger">
+      You are not authorized to access this.
+    </Alert>
   );
 };
 
@@ -276,6 +269,7 @@ const mapStateToProps = (state) => ({
   },
   isLoading: state.project.isLoading,
   selectedTeam: state.team.selectedTeam,
+  currentOrganization: state.organization,
 });
 
 export default React.memo(withRouter(connect(mapStateToProps, mapDispatchToProps)(CreateProjectPopup)));
