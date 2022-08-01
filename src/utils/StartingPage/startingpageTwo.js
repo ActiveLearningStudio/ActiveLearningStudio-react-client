7b9655ac401e6d8dd5ebebc6f788518b23427c5f
muhammadqamar <muhammadqamar111@gmail.com>
2022-08-01 14:10:25 +0500

    projects popup

--- src/utils/StartingPage/startingpageTwo.js
+++ src/utils/StartingPage/startingpageTwo.js
@@ -26,28 +26,43 @@ const StartingPageTwo = ({
   const { activeOrganization } = useSelector((state) => state.organization);
   return (
     <div className={currikiUtility}>
-      <div className="startpage-section help-section">
-        <a href="https://support.curriki.org/" className="section-btn ">
-          <svg width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M28 54C42.3594 54 54 42.3594 54 28C54 13.6406 42.3594 2 28 2C13.6406 2 2 13.6406 2 28C2 42.3594 13.6406 54 28 54Z"
-              stroke={primaryColor}
-              strokeWidth="4"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-            <path
-              d="M20.4336 20.2003C21.0449 18.4626 22.2514 16.9974 23.8395 16.064C25.4276 15.1307 27.2947 14.7895 29.1103 15.101C30.9258 15.4124 32.5725 16.3563 33.7588 17.7655C34.9451 19.1747 35.5943 20.9583 35.5916 22.8003C35.5916 28.0003 27.7916 30.6003 27.7916 30.6003"
-              stroke={primaryColor}
-              strokeWidth="4"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-            <path d="M28 41H28.0348" stroke={primaryColor} strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
-          </svg>
+      <div className="startpage-section">
+        <div className="create-project-card">
+          <div className="section-btn" onClick={onClick}>
+            <svg width="48" height="48" viewBox="0 0 52 52" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M2 26C2.03441 26 34.0143 26.0003 50 26.0005" stroke={primaryColor} strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+              <path d="M26 50C26 49.9656 26 17.9857 26 2" stroke={primaryColor} strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+            </svg>
+            <span>{createBtnTitle}</span>
+          </div>
 
-          <span>{helpBtnTitle}</span>
-        </a>
+          <div className="create-project-popup">
+            <h5 className="popup-heading">Create your First Project!</h5>
+            <p className="popup-discription">Projects enables authors to group Activities into a collection.</p>
+            <div className="divider" />
+            <h6 className="popup-use-project">You can use Projects to:</h6>
+
+            <div className="lists">
+              <svg width="14px" height="12px" viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M13 1.875L4.75 10.125L1 6.375" stroke={primaryColor} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+              </svg>
+              <p className="list-text">Organize Activities of a common topic into a folder.</p>
+            </div>
+            <div className="lists">
+              <svg width="14px" height="12px" viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M13 1.875L4.75 10.125L1 6.375" stroke={primaryColor} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+              </svg>
+              <p className="list-text">Create a set of Activities utilizing a Playlist.</p>
+            </div>
+            <div className="lists">
+              <svg width="14px" height="12px" viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M13 1.875L4.75 10.125L1 6.375" stroke={primaryColor} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+              </svg>
+
+              <p className="list-text">Assemble Activities and Playlists into a complete course.</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
