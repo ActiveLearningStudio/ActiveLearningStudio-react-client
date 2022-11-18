8453e965c36ea147174d76a91fc3fac3a733c5b4
Muhammad Qamar <muhammadqamar111@gmail.com>
2022-11-18 16:35:31 +0500

    FEAT:add new route komodo

--- src/containers/RecordVideo/index.js
+++ src/containers/RecordVideo/index.js
@@ -2,18 +2,15 @@
 /* eslint-disable react/button-has-type */
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
-import { Alert, Tabs, Tab } from 'react-bootstrap';
+import { Tabs, Tab } from 'react-bootstrap';
 import PageHeadline from './pageHeadline';
-import Image1 from '../../assets/images/record_1.png';
-import Image2 from '../../assets/images/record_2.png';
-import './style.scss';
-import LoginRecSMSvg from 'iconLibrary/mainContainer/LoginRecSMSvg';
 import { getGlobalColor } from 'containers/App/DynamicBrandingApply';
-import HelpRecSmSvg from 'iconLibrary/mainContainer/HelpRecSmSvg';
 import KomodoCard from 'utils/KomodoCard/komodocard';
 import { useDispatch, useSelector } from 'react-redux';
 import ProjectCardSkeleton from 'components/Skeletons/projectCard';
 import { getKomdoVideoList } from 'store/actions/komodo';
+import WelcomeScreen from './WelcomeScreen';
+import './style.scss';
 
 const RecordVideoPage = () => {
   const dispatch = useDispatch();
@@ -52,80 +49,33 @@ const RecordVideoPage = () => {
               >
                 <Tab eventKey="Komodo library" title="Komodo library">
                   <div className="komodo-lib-section">
-                    {true ? (
-                      <>
-                        {isLoading ? (
-                          <div className="d-flex ">
-                            <ProjectCardSkeleton />
-                            <ProjectCardSkeleton />
-                            <ProjectCardSkeleton />
-                          </div>
-                        ) : (
-                          <>
-                            <div className="row">
-                              {komodoList?.map((data) => (
-                                <>
-                                  <div className="mt-24">
-                                    <KomodoCard data={data} />
-                                  </div>
-                                </>
-                              ))}
-                            </div>
-                          </>
-                        )}
-                      </>
-                    ) : (
-                      <>
-                        <div className="komodo-lib">
-                          <div className="komodo-lib-left">
-                            <h2>Hello!</h2>
-                            <h3>Now you can record a video via Komodo</h3>
-                            <div className="lib-left-detail">
-                              <p>Komodo helps you collaborate faster and share your ideas with people without the need to type lots of text, and scheduling online meetings.</p>
-                              <div className="sub-two">
-                                <p>Learn more about Komodo &nbsp; </p>
-                                <a href="#">here</a>
-                              </div>
-                            </div>
-                          </div>
-                          <div className="komodo-lib-right">
-                            <div className="lib-right-div mb-90">
-                              <div>
-                                <img src={Image1} alt="Image1" />
-                              </div>
-                              <div className="lib-div-inner">
-                                <h3>Get started</h3>
-                                <p>Link Komodo features to your CurrikiStudio account and start creating.</p>
-                                <button>
-                                  <LoginRecSMSvg primaryColor={primaryColor} />
-                                  Log in / Sign up
-                                </button>
-                              </div>
-                            </div>
-                            <div className="lib-right-div">
-                              <div>
-                                <img src={Image2} alt="Image1" />
-                              </div>
-                              <div className="lib-div-inner">
-                                <h3>Get started</h3>
-                                <ol className="lib-div-inner-ul">
-                                  <li>
-                                    <p>Record your screen, Cam and talk about whatever you want. </p>
-                                  </li>
-                                  <li>
-                                    <p>Once you finish your recording, copy the link and share it.</p>
-                                  </li>
-                                </ol>
-                                <button>
-                                  <HelpRecSmSvg primaryColor={primaryColor} />
-                                  Learn more
-                                </button>
-                              </div>
-                            </div>
-                          </div>
+                    <>
+                      {isLoading ? (
+                        <div className="d-flex ">
+                          <ProjectCardSkeleton />
+                          <ProjectCardSkeleton />
+                          <ProjectCardSkeleton />
                         </div>
-                      </>
-                    )}
+                      ) : (
+                        <>
+                          {komodoList?.length !== 0 ? (
+                            <>
+                              <div className="row">
+                                {komodoList?.map((data) => (
+                                  <>
+                                    <div className="mt-16">
+                                      <KomodoCard data={data} />
+                                    </div>
+                                  </>
+                                ))}
+                              </div>
+                            </>
+                          ) : (
+                            <WelcomeScreen />
+                          )}
+                        </>
+                      )}
+                    </>
                   </div>
                 </Tab>
               </Tabs>
