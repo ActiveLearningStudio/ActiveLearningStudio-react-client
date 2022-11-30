0d31389e691dd34fea24b7b5dfcb2dfda602773e
aqibnwaz <aqib@curriki.org>
2022-11-30 17:26:23 +0500

    fix ui for video activity

--- src/containers/RecordVideo/index.js
+++ src/containers/RecordVideo/index.js
@@ -52,102 +52,103 @@ const RecordVideoPage = () => {
   };
   const primaryColor = getGlobalColor('--main-primary-color');
   return (
-    <div className="record-content-wrapper">
-      <div className=" inner-content">
-        {permission?.['Record a Video']?.includes('record-video:view') ? (
-          <>
-            <div className="content-wrapper record-content-wrapper">
-              <div className="inner-content">
-                <div className="record-page-section">
-                  <PageHeadline />
-                  <div className="video-cards-top-search-filter">
-                    <div className="search-bar-clear-btn">
-                      <div className="search-bar">
-                        <input
-                          className=""
-                          type="text"
-                          value={searchQuery}
-                          onChange={(e) => {
-                            setSearchQuery(e.target.value);
-                          }}
-                          placeholder="Search recorded videos"
-                        />
-                        <SearchInputMdSvg
-                          primaryColor={primaryColor}
-                          onClick={() => {
-                            if (currentOrganization) {
-                              dispatch(getKomdoVideoList(currentOrganization?.id, ActivePage, 30, searchQuery));
-                            }
-                          }}
-                          style={{ cursor: 'pointer' }}
-                        />
-                      </div>
-                      <div>
-                        <Buttons
-                          text="Clear"
-                          className="clr-btn"
-                          onClick={() => {
-                            setSearchQuery('');
-                            setActivePage(1);
-                            dispatch(getKomdoVideoList(currentOrganization?.id, 1, 30, ''));
-                          }}
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="tab-div">
-                    <Tabs
-                      onSelect={(eventKey) => {
-                        setType('sample');
+    <>
+      <div className="content-wrapper record-content-wrapper">
+        <div className="inner-content">
+          {permission?.['Record a Video']?.includes('record-video:view') ? (
+            <div className="record-page-section">
+              <PageHeadline />
+              <div className="video-cards-top-search-filter">
+                <div className="search-bar-clear-btn">
+                  <div className="search-bar">
+                    <input
+                      className=""
+                      type="text"
+                      value={searchQuery}
+                      onChange={(e) => {
+                        setSearchQuery(e.target.value);
                       }}
-                      className="main-tabs"
-                      defaultActiveKey={activeTab}
-                      id="uncontrolled-tab-example"
-                    >
-                      <Tab eventKey="Komodo library" title="Komodo library">
-                        <div className="komodo-lib-section">
-                          <>
-                            {!komodoVideoList ? (
-                              <div className="d-flex ">
-                                <ProjectCardSkeleton />
-                                <ProjectCardSkeleton />
-                                <ProjectCardSkeleton />
-                              </div>
-                            ) : (
-                              <>
-                                {komodoVideoList?.data?.length ? (
-                                  <div className="row">
-                                    {komodoVideoList?.data?.map((data) => (
-                                      <div className="mt-16">
-                                        <KomodoCard
-                                          data={data}
-                                          setModalShow={setShow}
-                                          permission={permission}
-                                          // setCurrentActivity={setCurrentActivity}
-                                        />
-                                      </div>
-                                    ))}
-                                    {ActivePage !== 1 && isLazyLoading && (
-                                      <div className="col-md-12 mt-3 text-center">
-                                        <ImgLoader />
-                                      </div>
-                                    )}
-                                  </div>
-                                ) : (
-                                  <WelcomeScreen />
-                                )}
-                              </>
-                            )}
-                          </>
-                        </div>
-                      </Tab>
-                    </Tabs>
+                      placeholder="Search recorded videos"
+                    />
+                    <SearchInputMdSvg
+                      primaryColor={primaryColor}
+                      onClick={() => {
+                        if (currentOrganization) {
+                          dispatch(getKomdoVideoList(currentOrganization?.id, ActivePage, 30, searchQuery));
+                        }
+                      }}
+                      style={{ cursor: 'pointer' }}
+                    />
+                  </div>
+                  <div>
+                    <Buttons
+                      text="Clear"
+                      className="clr-btn"
+                      onClick={() => {
+                        setSearchQuery('');
+                        setActivePage(1);
+                        dispatch(getKomdoVideoList(currentOrganization?.id, 1, 30, ''));
+                      }}
+                    />
                   </div>
                 </div>
               </div>
+              <div className="tab-div">
+                <Tabs
+                  onSelect={(eventKey) => {
+                    setType('sample');
+                  }}
+                  className="main-tabs"
+                  defaultActiveKey={activeTab}
+                  id="uncontrolled-tab-example"
+                >
+                  <Tab eventKey="Komodo library" title="Komodo library">
+                    <div className="komodo-lib-section">
+                      <>
+                        {!komodoVideoList ? (
+                          <div className="d-flex ">
+                            <ProjectCardSkeleton />
+                            <ProjectCardSkeleton />
+                            <ProjectCardSkeleton />
+                          </div>
+                        ) : (
+                          <>
+                            {komodoVideoList?.data?.length ? (
+                              <div className="row">
+                                {komodoVideoList?.data?.map((data) => (
+                                  <div className="mt-16">
+                                    <KomodoCard
+                                      data={data}
+                                      setModalShow={setShow}
+                                      permission={permission}
+                                      // setCurrentActivity={setCurrentActivity}
+                                    />
+                                  </div>
+                                ))}
+                                {ActivePage !== 1 && isLazyLoading && (
+                                  <div className="col-md-12 mt-3 text-center">
+                                    <ImgLoader />
+                                  </div>
+                                )}
+                              </div>
+                            ) : (
+                              <WelcomeScreen />
+                            )}
+                          </>
+                        )}
+                      </>
+                    </div>
+                  </Tab>
+                </Tabs>
+              </div>
             </div>
-            <MyActivity playlistPreview activityPreview redirecttoactivity />
-            {/* <Modal size="lg" aria-labelledby="contained-modal-title-vcenter" centered show={show} onHide={handleClose}>
+          ) : (
+            <Alert variant="danger">You are not authorized to view this page.</Alert>
+          )}
+        </div>
+      </div>
+      <MyActivity playlistPreview activityPreview redirecttoactivity />
+      {/* <Modal size="lg" aria-labelledby="contained-modal-title-vcenter" centered show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>{currentActivity.title}</Modal.Title>
         </Modal.Header>
@@ -157,12 +158,7 @@ const RecordVideoPage = () => {
           </video>
         </Modal.Body>
       </Modal> */}
-          </>
-        ) : (
-          <Alert variant="danger">You are not authorized to view this page.</Alert>
-        )}
-      </div>
-    </div>
+    </>
   );
 };
 
