ae40bb7f6cc928c30d677db5107b67015b38aeba
muhammadqamar <muhammadqamar111@gmail.com>
2022-12-07 13:52:08 +0500

    added learnsafe API

--- src/containers/LMS/LTI/Activity/index.js
+++ src/containers/LMS/LTI/Activity/index.js
@@ -204,7 +204,12 @@ const Activity = (props) => {
           const jsonStatement = JSON.stringify(xapiData);
           sendStatement(jsonStatement);
           if (h5pSettings?.organization?.api_key) {
-            sendScreenshot(h5pSettings.organization, jsonStatement, h5pSettings.activity.title, params.studentId);
+            let StudentNameCustom = '';
+            if (customPersonNameGiven) {
+              StudentNameCustom = customPersonNameGiven;
+            }
+
+            sendScreenshot(h5pSettings.organization, jsonStatement, h5pSettings.activity.title, StudentNameCustom || params.studentId);
           }
         }
       });
